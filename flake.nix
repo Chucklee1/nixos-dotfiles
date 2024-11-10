a60645605ffded7c81240a358cb8ca9ef65ddbb5
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2024-11-09 20:25:46 -0800

    created flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "i dont kow what im doing";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+    home-manager.url = "github:nix-community/home-manager";  
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";  
+    stylix.url = "github:danth/stylix";  
+  };
+
+  outputs = { self, nixpkgs, home-manager, stylix, ... } @ inputs: {
+  
+    # caprine - macbook profile
+    nixosConfigurations.caprine = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux"; 
+      modules = [
+        ./hosts/caprine/configuration.nix
+        stylix.nixosModules.stylix  
+        home-manager.nixosModules.home-manager  
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.users.caprine = import ./hosts/caprine/home.nix;
+        }
+      ];
+    };
+    # goat - desktop profile
+    nixosConfigurations.goat = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux"; 
+      modules = [
+        ./hosts/goat/configuration.nix
+        stylix.nixosModules.stylix  
+        home-manager.nixosModules.home-manager  
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.users.caprine = import ./hosts/goat/home.nix;
+        }
+      ];
+    };
+  }; 
+}
