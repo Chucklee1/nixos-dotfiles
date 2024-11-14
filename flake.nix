05f1babf52924ca920ae8e6e965047bc91f7df31
installer <installer@gmail.com>
2024-11-13 20:03:15 -0800

    temp remove profile caprine

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         ./hardware/goat-hardware-configuration.nix
         ./modules/default-config.nix
         inputs.home-manager.nixosModules.home-manager
-        inputs.stylix.nixosModules.stylix
+        inputs.niri.homeModules.niri
         inputs.niri.nixosModules.niri
         {
           # modules
@@ -41,47 +41,13 @@
             backupFileExtension = "backup";
             users = {
               goat = {
-                imports = [./home/default-home.nix];
+                imports = [
+                  ./home/default-home.nix
+                  inputs.niri.homeModules.niri
+                ];
                 home.username = "goat";
                 home.homeDirectory = "/home/goat";
                 programs.niri.enable = true;
-              };
-            };
-          };
-        }
-      ];
-    };
-    # caprine - macbook profile
-    nixosConfigurations.caprine = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hardware/caprine-hardware-configuration.nix
-        ./modules/default-config.nix
-        ./modules/niri.nix
-        inputs.home-manager.nixosModules.home-manager
-        inputs.stylix.nixosModules.stylix
-        inputs.niri.nixosModules.niri
-        {
-          # modules
-          nvidia.enable = false;
-          # user hostname
-          networking.hostName = "caprine";
-          # system user config
-          users.users.caprine = {
-            isNormalUser = true;
-            extraGroups = ["wheel" "networkmanager"];
-          };
-          # home manager user config
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            backupFileExtension = "backup";
-            users = {
-              caprine = {
-                imports = [./home/default-home.nix];
-                home.username = "caprine";
-                home.homeDirectory = "/home/caprine";
               };
             };
           };
