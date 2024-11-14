d6b84afb5c61b5d3ab98b97ebcf153b7a23d84b8
installer <installer@gmail.com>
2024-11-13 19:59:58 -0800

    merged system and default config

--- modules/default-config.nix
+++ modules/default-config.nix
@@ -10,6 +10,31 @@
     ./system.nix
     ./theming.nix
   ];
+  # D O  N O T  C H A N G E
+  system.stateVersion = "24.05";
+
+  # boot loader
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    plymouth.enable = true;
+  };
+
+  # tty settings
+  console.font = "Lat2-Terminus16";
+  console.useXkbConfig = true;
+
+  # nix
+  nixpkgs.config.allowUnfree = true;
+  nix.settings = {
+    auto-optimise-store = true;
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  # idk
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Vancouver";
+  i18n.defaultLocale = "en_CA.UTF-8";
 
   # env packages
   environment.systemPackages = with pkgs; [
