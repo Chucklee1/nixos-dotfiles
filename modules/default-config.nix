3d15752c0af482cfe11cde05dc962ad939b9c518
installer <installer@gmail.com>
2024-11-15 18:26:20 -0800

    created a seperate file for services, hardware, programs, and packages, also cleaned default config. naming is hard...

--- modules/default-config.nix
+++ modules/default-config.nix
@@ -7,12 +7,17 @@
 }: {
   imports = [
     ./GPU/nvidia.nix # toggle module
+    ./infsasoftware.nix
     ./theming.nix
     inputs.home-manager.nixosModules.home-manager
     inputs.stylix.nixosModules.stylix
     inputs.niri.nixosModules.niri
   ];
 
+  # D O  N O T  C H A N G E
+  system.stateVersion = "24.05";
+
+  # boot loader
   boot.loader = {
     efi.canTouchEfiVariables = true;
     efi.efiSysMountPoint = "/boot";
@@ -21,10 +26,6 @@
     grub.device = "nodev";
   };
 
-  # D O  N O T  C H A N G E
-  system.stateVersion = "24.05";
-
-  # boot load
   # tty settings
   console.font = "Lat2-Terminus16";
   console.useXkbConfig = true;
@@ -34,10 +35,8 @@
     config.allowUnfree = true;
     overlays = [inputs.niri.overlays.niri];
   };
-  nix.settings = {
-    auto-optimise-store = true;
-    experimental-features = ["nix-command" "flakes"];
-  };
+  nix.settings.auto-optimise-store = true;
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # idk
   networking.networkmanager.enable = true;
@@ -61,113 +60,10 @@
     };
   };
 
-  # env packages
-  environment.systemPackages = with pkgs; [
-    # Development Tools
-    ripgrep
-    alejandra
-    nixd
-    libgccjit
-    rustc
-
-    # Command-Line Utilities
-    killall
-    pciutils
-    sl
-    cowsay
-    neofetch
-
-    # Web & Networking Utilities
-    wget
-    git
-    curl
-    networkmanagerapplet
-
-    # Compression & Archiving
-    unrar
-    unzip
-    file-roller
-    tree
-    isoimagewriter
-
-    # Wayland & Display Utilities
-    wayland
-    wayland-protocols
-    wayland-utils
-    wayland-scanner
-    egl-wayland
-    qt5.qtwayland
-    qt6.qtwayland
-
-    # Clipboard & Clipboard Management
-    wl-clipboard
-    cliphist
-    xclip
-
-    # Security & Authentication
-    libsecret
-    lxqt.lxqt-policykit
-
-    # Media Tools
-    mpv
-    imv
-    ffmpeg
-    v4l-utils
-
-    # Keyboard & Input Tools
-    wev
-    ydotool
-    wtype
-
-    # System Controls
-    playerctl
-    pavucontrol
-    brightnessctl
-    wlsunset
-  ];
-
-  # programs
-  programs = {
-    thunar.enable = true;
-    thunar.plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-volman
-    ];
-    seahorse.enable = true; # password app
-    niri.enable = true;
-    niri.package = pkgs.niri-unstable;
-  };
-
-  # opengl option, renamed to graphics as of 24.11
-  hardware.graphics.enable = true;
-
-  # bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
-
-  # sound
-  hardware.pulseaudio.enable = false;
-  services.pipewire = {
+  # overlay packages
+  programs.niri = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # display-manager
-  services.displayManager = {
-    enable = true;
-    ly.enable = true;
-  };
-  # misc services
-  services = {
-    printing.enable = true;
-    gvfs.enable = true;
-    tumbler.enable = true;
-    fstrim.enable = true;
-    gnome.gnome-keyring.enable = true;
-    openssh.enable = true;
+    package = pkgs.niri-unstable;
   };
 
   # security
---
+++ modules/infsasoftware.nix
@@ -0,0 +1,108 @@
+{pkgs, ...}: {
+  # env packages
+  environment.systemPackages = with pkgs; [
+    # Development Tools
+    ripgrep
+    alejandra
+    nixd
+    libgccjit
+    rustc
+
+    # Command-Line Utilities
+    killall
+    pciutils
+    sl
+    cowsay
+    neofetch
+
+    # Web & Networking Utilities
+    wget
+    git
+    curl
+    networkmanagerapplet
+
+    # Compression & Archiving
+    unrar
+    unzip
+    file-roller
+    tree
+    isoimagewriter
+
+    # Wayland & Display Utilities
+    wayland
+    wayland-protocols
+    wayland-utils
+    wayland-scanner
+    egl-wayland
+    qt5.qtwayland
+    qt6.qtwayland
+
+    # Clipboard & Clipboard Management
+    wl-clipboard
+    cliphist
+    xclip
+
+    # Security & Authentication
+    libsecret
+    lxqt.lxqt-policykit
+
+    # Media Tools
+    mpv
+    imv
+    ffmpeg
+    v4l-utils
+
+    # Keyboard & Input Tools
+    wev
+    ydotool
+    wtype
+
+    # System Controls
+    playerctl
+    pavucontrol
+    brightnessctl
+    wlsunset
+  ];
+
+  # programs
+  programs = {
+    thunar.enable = true;
+    thunar.plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+    seahorse.enable = true; # password app
+  };
+
+  # opengl option, renamed to graphics as of 24.11
+  hardware.graphics.enable = true;
+
+  # bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # sound
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # display-manager
+  services.displayManager = {
+    enable = true;
+    ly.enable = true;
+  };
+  # misc services
+  services = {
+    printing.enable = true;
+    gvfs.enable = true;
+    tumbler.enable = true;
+    fstrim.enable = true;
+    gnome.gnome-keyring.enable = true;
+    openssh.enable = true;
+  };
+}
