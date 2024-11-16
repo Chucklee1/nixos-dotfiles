b5fe8c4e7a343dba7f50b872c78cead1dac305c9
installer <installer@gmail.com>
2024-11-16 13:47:51 -0800

    moved programs into programs scope, which was the not the case bellow, try to see if I can put programs with home-manager block but I will probably need to undo the change after this commit

--- modules/infasoftware.nix
+++ modules/infasoftware.nix
@@ -78,60 +78,58 @@
   };
 
   # home manager programs
-  home-manager.users.goat = {
+  home-manager.users.goat.programs = {
     lazygit.enable = true;
     wlogout.enable = true;
     fuzzel.enable = true;
-    programs = {
-      kitty = {
-        enable = true;
-        settings = {
-          scrollback_lines = 2000;
-          wheel_scroll_min_lines = 1;
-          window_padding_width = 4;
-          confirm_os_window_close = 0;
-        };
-        extraConfig = ''
-          tab_bar_style fade
-          tab_fade 1
-          active_tab_font_style   bold
-          inactive_tab_font_style bold
-        '';
+    kitty = {
+      enable = true;
+      settings = {
+        scrollback_lines = 2000;
+        wheel_scroll_min_lines = 1;
+        window_padding_width = 4;
+        confirm_os_window_close = 0;
       };
-      oh-my-posh = {
-        enable = true;
-        enableBashIntegration = true;
-        useTheme = "pure";
-      };
-      neovim = {
-        enable = true;
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
-        extraConfig = ''
-          set clipboard=unnamedplus
-          set number
-          set tabstop=2
-          set shiftwidth=2
-          set expandtab  " Use spaces instead of tabs
-        '';
-      };
-      git = {
-        enable = true;
-        userEmail = "cooperkang4@gamil.com";
-        userName = "Chucklee1";
-      };
-      bash = {
-        enable = true;
-        shellAliases = {
-          sv = "sudo nvim";
-          v = "nvim";
-          exec-swww = "swww init && swww img ~/nixos-dotfiles/wallpapers/mono-forest.PNG";
-          ozonify = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
-          cg = "sudo nix-collect-garbage";
-          update-desktop = "sudo nixos-rebuild switch --flake .#desktop --show-trace";
-          update-macbook = "sudo nixos-rebuild switch --flake .#macbook --show-trace";
-        };
+      extraConfig = ''
+        tab_bar_style fade
+        tab_fade 1
+        active_tab_font_style   bold
+        inactive_tab_font_style bold
+      '';
+    };
+    oh-my-posh = {
+      enable = true;
+      enableBashIntegration = true;
+      useTheme = "pure";
+    };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      extraConfig = ''
+        set clipboard=unnamedplus
+        set number
+        set tabstop=2
+        set shiftwidth=2
+        set expandtab  " Use spaces instead of tabs
+      '';
+    };
+    git = {
+      enable = true;
+      userEmail = "cooperkang4@gamil.com";
+      userName = "Chucklee1";
+    };
+    bash = {
+      enable = true;
+      shellAliases = {
+        sv = "sudo nvim";
+        v = "nvim";
+        exec-swww = "swww init && swww img ~/nixos-dotfiles/wallpapers/mono-forest.PNG";
+        ozonify = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+        cg = "sudo nix-collect-garbage";
+        update-desktop = "sudo nixos-rebuild switch --flake .#desktop --show-trace";
+        update-macbook = "sudo nixos-rebuild switch --flake .#macbook --show-trace";
       };
     };
   };
