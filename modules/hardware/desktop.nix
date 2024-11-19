b6b8091bd32b5fac43789a761a825432c85f231b
installer <installer@gmail.com>
2024-11-19 15:59:50 -0800

    whops

--- modules/hardware/desktop.nix
+++ modules/hardware/desktop.nix
@@ -9,7 +9,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot = {
+  boot = { 
     initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
     initrd.kernelModules = [];
     kernelModules = ["kvm-amd"];
@@ -17,33 +17,31 @@
     supportedFilesystems = ["ntfs"]; 
   };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/458806b5-8214-4b19-b163-a74016bf29fe";
-    fsType = "ext4";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/458806b5-8214-4b19-b163-a74016bf29fe";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/1C7A-AD30";
+      fsType = "vfat";
+      options = ["fmask=0077" "dmask=0077"];
+    };
+
+      # other drives
+    "/run/media/goat/BLUE-SATA" = {
+      device = "/dev/disk/by-uuid/C814039D14038D9E";
+      fsType = "ntfs";
+    };
+    "/run/media/goat/SATA-DRIVE-1" = {
+      device = "/dev/disk/by-uuid/2CB23BB6B23B837E   ";
+      fsType = "ntfs";
+    }; 
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/1C7A-AD30";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
-  };
-
-  # other drives
-  fileSystems."/run/media/goat/BLUE-SATA" = {
-    device = "/dev/disk/by-uuid/C814039D14038D9E";
-    fsType = "ntfs";
-  };
-  fileSystems."/run/media/goat/SATA-DRIVE-1" = {
-    device = "/dev/disk/by-uuid/2CB23BB6B23B837E   ";
-    fsType = "ntfs";
-  };
-  
   swapDevices = [];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp7s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp6s0.useDHCP = lib.mkDefault true;
