e4dc197bcc26d17aa23635f5adfc854c4107f80b
installer <installer@gmail.com>
2024-11-20 16:20:04 -0800

    removed and reorginized modules

--- modules/wayland/hyprland/hyprland.nix
+++ modules/wayland/hyprland/hyprland.nix
@@ -7,20 +7,12 @@
   options = {hyprland.enable = lib.mkEnableOption "enable hyprland window manager module";};
 
   config = lib.mkIf config.hyprland.enable {
-    # system hyprland 
     programs.hyprland = {
       xwayland.enable = true;
       enable = true; 
       portalPackage = pkgs.xdg-desktop-portal-hyprland;
     };
-    # home manager
-    home-manager.users.goat = {
-      stylix.targets.hyprland.enable = true;
-      wayland.windowManager.hyprland = {
-        plugins = pkgs.hyprlandPlugins.hyprscroller;
-      };
-    };
-    # system packages
+    home-manager.users.goat.stylix.targets.hyprland.enable = true;
     environment.systemPackages = pkgs.hyprland-protocols;
   };
 }
--- modules/wayland/niri/niri.nix
+++ modules/wayland/niri/niri.nix
@@ -10,20 +10,16 @@
   config = lib.mkIf config.niri.enable {
     # package extension
     nixpkgs.overlays = [inputs.niri.overlays.niri];
-    programs.niri = {
-      enable = true;
-      package = pkgs.niri-unstable;
-    };
-    # home manager
+    programs.niri.enable = true;
+    programs.niri.package = pkgs.niri-unstable;
+    
     home-manager.users.goat = {
       # nested niri.settings so config.lib.niri.actions will work
       imports = [./settings.nix];
       stylix.targets.niri.enable = true;
     };
     # gtk portal
-    xdg.portal = {
-      extraPortals = pkgs.xdg-desktop-portal-gtk;
-      configPackages = pkgs.xdg-desktop-portal-gtk;
-    };
+    xdg.portal.extraPortals = pkgs.xdg-desktop-portal-gtk;
+    xdg.portal.configPackages = pkgs.xdg-desktop-portal-gtk;
   };
 }
