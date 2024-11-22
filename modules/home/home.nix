0d9e2ee7e2233f9709daead37c6c6fdd2467f9f8
Chucklee1 <cooperkang4@gamil.com>
2024-11-21 17:59:36 -0800

    updated alias

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -91,7 +91,7 @@
         ozonify = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
         cg = "sudo nix-collect-garbage";
         update-desktop = "sudo nixos-rebuild switch --flake .#desktop --show-trace";
-        update-macbook = "sudo nixos-rebuild switch --flake .#macbook --show-trace";
+        update-laptop = "sudo nixos-rebuild switch --flake .#laptop --show-trace";
       };
     };
   };
