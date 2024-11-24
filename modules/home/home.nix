21dbde385bbb05588c6bb88a471f6f5a92d87762
Chucklee1 <cooperkang4@gamil.com>
2024-11-24 10:33:18 -0800

    removed wlogout style, added theming for qt and gtk

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -102,8 +102,21 @@
   # theming
   # -----------------------------------------------------------
   gtk = {
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+  qt = {
     enable = true;
-    iconTheme.name = "Papirus Dark";
+    style.name = "adwaita-dark";
+    platformTheme.name = "gtk3";
   };
   stylix.targets = {
     neovim.enable = true;
