288cd42f48ef873b589d7b786436b254b0eeac8d
Chucklee1 <cooperkang4@gamil.com>
2024-11-29 19:08:01 -1000

    added option to turn off minimap, added waybar switch to sh script

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -46,6 +46,9 @@
         jnoortheen.nix-ide
         eamodio.gitlens
       ];
+      settings = {
+        "editor.minimap.enabled" = false;
+      };
     };
     waybar = {
       enable = true;
@@ -94,7 +97,7 @@
       shellAliases = {
         sv = "sudo nvim";
         v = "nvim";
-        start-pls = "$HOME/nixos-dotfiles/modules/home/set-home.sh";
+        start-pls = ". $HOME/nixos-dotfiles/modules/home/set-home.sh";
         kittty = "kitty working-directory $HOME/nixos-dotfiles";
         ozonify = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
         cg = "sudo nix-collect-garbage";
--- modules/home/set-home.sh
+++ modules/home/set-home.sh
@@ -4,6 +4,8 @@
 rm -rf $HOME/.config/waybar/*
 ln -s $HOME/nixos-dotfiles/modules/home/waybar/config.jsonc $HOME/.config/waybar/
 ln -s $HOME/nixos-dotfiles/modules/home/waybar/style.css $HOME/.config/waybar/
+pkill waybar
+waybar &
 
 # swww
 swww img $HOME/nixos-dotfiles/pictures/mono-forest.PNG
