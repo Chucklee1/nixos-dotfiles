72c312d1e07bd6f133569de5d02592231567c341
Chucklee1 <cooperkang4@gamil.com>
2024-11-29 16:21:07 -1000

    added impermance for home manager, rename symlink script to wipe one

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    impermanence.url = "github:nix-community/impermanence";
     stylix.url = "github:danth/stylix";
     niri.url = "github:sodiboo/niri-flake";
     grub2-themes.url = "github:vinceliuice/grub2-themes";
@@ -13,43 +14,56 @@
   outputs = {
     self,
     nixpkgs,
+    impermanence,
     grub2-themes,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    shared-modules = [
+      ./modules/default.nix
+      inputs.home-manager.nixosModules.home-manager
+      inputs.stylix.nixosModules.stylix
+      inputs.niri.nixosModules.niri
+      grub2-themes.nixosModules.default
+      {
+        nixpkgs.overlays = [inputs.niri.overlays.niri];
+        programs.niri.enable = true;
+        programs.niri.package = pkgs.niri-unstable;
+        home-manager = {
+          useUserPackages = true;
+          useGlobalPkgs = true;
+          extraSpecialArgs = {inherit inputs;};
+          users.goat.imports = [./modules/home/home.nix];
+        };
+      }
+    ];
+  in {
     # desktop profile
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
-      modules = [
-        # flake inputs
-        inputs.home-manager.nixosModules.home-manager
-        inputs.stylix.nixosModules.stylix
-        inputs.niri.nixosModules.niri
-        grub2-themes.nixosModules.default
-        ./modules/default.nix
-        ./modules/hardware/desktop.nix
-        {
-          nvidia.enable = true;
-        }
-      ];
+      modules =
+        shared-module
+        ++ [
+          ./modules/hardware/desktop.nix
+          {
+            nvidia.enable = true;
+          }
+        ];
     };
     # laptop profile
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
-      modules = [
-        # flake inputs
-        inputs.home-manager.nixosModules.home-manager
-        inputs.stylix.nixosModules.stylix
-        inputs.niri.nixosModules.niri
-        grub2-themes.nixosModules.default
-        ./modules/default.nix
-        ./modules/hardware/laptop.nix
-        {
-          services.xserver.videoDrivers = ["amd"];
-          nvidia.enable = false;
-        }
-      ];
+      modules =
+        shared-module
+        ++ [
+          impermanence.homeManagerModules.impermanence
+          ./modules/hardware/laptop.nix
+          {
+            services.xserver.videoDrivers = ["amd"];
+            nvidia.enable = false;
+          }
+        ];
     };
   };
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,21 +1,12 @@
-{inputs, pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     ./nixos/nvidia.nix
     ./nixos/gamse.nix
     ./nixos/system.nix
     ./nixos/infasoftware.nix
   ];
-
-  # must be put in global scope for hm niri to work
-  nixpkgs.overlays = [inputs.niri.overlays.niri];
-  programs.niri.enable = true;
-  programs.niri.package = pkgs.niri-unstable;
-  
-  # global scope for hm
-  home-manager = {
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    extraSpecialArgs = {inherit inputs;};
-    users.goat.imports = [./home/home.nix];
-  };
 }
