1e15f35552643aab497a22e5aeecd958a3bce983
Chucklee1 <cooperkang4@gamil.com>
2024-12-04 13:25:52 -0800

    updated path names

--- modules/default.nix
+++ modules/default.nix
@@ -8,23 +8,23 @@
     ./system/home-manager.nix
     ./system/specifics.nix
     ./system/user.nix
-    ./system/infastructure.nix
-    ./system/packages.nix
-    ./system/theming.nix
 
-    ./programs/games.nix
+    ./bundles/infastructure.nix
+    ./bundles/packages.nix
+    ./bundles/theming.nix
+    ./bundles/games.nix
+    ./bundles/wayland.nix #
+
     ./programs/thunar.nix
     ./programs/bash.nix
     ./programs/git.nix
     ./programs/kitty.nix
     ./programs/neovim.nix
+    ./programs/niri.nix #
+    ./programs/vscode.nix #
 
-    # toggle modules #
-    ./system/wayland.nix
-    ./drivers/nvidia.nix
-    ./drivers/nvidia-wayland.nix
-    ./drivers/radeon.nix
-    ./programs/niri.nix
-    ./programs/vscode.nix
+    ./drivers/nvidia.nix #
+    ./drivers/nvidia-wayland.nix #
+    ./drivers/radeon.nix #
   ];
 }
