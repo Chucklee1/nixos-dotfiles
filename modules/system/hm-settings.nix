928369844f5ca9906cebb2668a8027b8d7cd1df5
Chucklee1 <cooperkang4@gamil.com>
2024-12-05 13:49:54 -0800

    moved users for hm to user module

--- modules/system/hm-settings.nix
+++ modules/system/hm-settings.nix
@@ -3,10 +3,5 @@
     useUserPackages = true;
     useGlobalPkgs = true;
     extraSpecialArgs = {inherit inputs;};
-    users.goat = {
-      home.stateVersion = "24.05"; # DO NOT CHANGE
-      home.username = "goat";
-      home.homeDirectory = "/home/goat";
-    };
   };
 }
--- modules/system/user.nix
+++ modules/system/user.nix
@@ -3,4 +3,9 @@
     isNormalUser = true;
     extraGroups = ["wheel" "networkmanager"];
   };
+  home-manager.users.goat = {
+    home.stateVersion = "24.05"; # DO NOT CHANGE
+    home.username = "goat";
+    home.homeDirectory = "/home/goat";
+  };
 }
