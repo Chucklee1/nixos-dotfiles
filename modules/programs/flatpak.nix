5efd6ffc0f90715f9ba59d641d145a760a268d50
Chucklee1 <cooperkang4@gamil.com>
2024-12-05 17:21:00 -0800

    did not work

--- modules/programs/flatpak.nix
+++ modules/programs/flatpak.nix
@@ -3,19 +3,12 @@
   config,
   ...
 }: {
-  services.flatpak = {
-    enable = true;
-    packages = ["com.valvesoftware.Steam"];
-    removeUnmanagedPackages = true;
-    remote = {
-      name = "flathub";
-      url = "https://flathub.org/repo/flathub.flatpakrepo";
-    };
-  };
-  systemd.services.flatpak-var = {
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
     wantedBy = ["multi-user.target"];
     path = [pkgs.flatpak];
     script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
       export XDG_DATA_DIRS="$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
     '';
   };
