5c3c2fabc00a24f13a1e1525e86cfe35456d8812
Chucklee1 <cooperkang4@gamil.com>
2024-12-06 14:45:57 -0800

    upated aliases

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -5,13 +5,10 @@
         enable = true;
         shellAliases = {
           sv = "sudo nvim";
-          v = "nvim";
-          start-pls = ". $HOME/nixos-dotfiles/dotconfig/set-home.sh";
-          kittty = "kitty working-directory $HOME/nixos-dotfiles";
-          ozonify = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
           cg = "sudo nix-collect-garbage";
-          update-desktop = "sudo nixos-rebuild switch --flake .#desktop --impure --show-trace";
-          update-laptop = "sudo nixos-rebuild switch --flake .#laptop --impure --show-trace";
+          update = "sudo nixos-rebuild switch --impure --show-trace --flake";
+          laptop = ".#laptop";
+          desktop = ".#desktop";
         };
       };
     }
