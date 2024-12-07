dde78594fcdf866dacf32960cae4ac445614a69f
Chucklee1 <cooperkang4@gamil.com>
2024-12-06 17:11:37 -0800

    enabled niri in flake

--- flake.nix
+++ flake.nix
@@ -39,9 +39,8 @@
           {
             vscode.enable = true;
             waybar.enable = true;
-            #niri.enable = true;
+            niri.enable = true;
             nvidia.enable = true;
-            #niri-nvidia.enable = true;
             radeon.enable = false;
           }
         ];
@@ -59,9 +58,8 @@
           {
             vscode.enable = true;
             waybar.enable = true;
-            #niri.enable = true;
+            niri.enable = true;
             nvidia.enable = false;
-            #niri-nvidia.enable = false;
             radeon.enable = true;
           }
         ];
