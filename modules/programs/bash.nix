3e76c26f9f65972d0e9c3f4af97d75e9a1da7ac7
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2024-12-09 01:58:30 -0800

    Update bash.nix
    more aliases

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -6,8 +6,10 @@
         shellAliases = {
           sv = "sudo nvim";
           cg = "sudo nix-collect-garbage";
-          update-laptop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#laptop";
-          update-desktop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#desktop";
+          update-laptop = "sudo nixos-rebuild switch --impure --show-trace --flake ./nixos-dotfiles#laptop";
+          update-desktop = "sudo nixos-rebuild switch --impure --show-trace --flake ./nixos-dotfiles#desktop";
+          remote-update-laptop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#laptop";
+          remote-update-desktop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#desktop";
         };
       };
     }
