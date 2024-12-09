93d2ca44eebd97ff25e1bd7c25a4312e544cb126
Chucklee1 <cooperkang4@gamil.com>
2024-12-09 13:44:29 -0800

    redid partition for minimal

--- modules/hardware/minimal.nix
+++ modules/hardware/minimal.nix
@@ -35,29 +35,27 @@
     # When using disko-install, we will overwrite this value from the commandline
     device = "/dev/disk/by-id/some-disk-id";
     type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        MBR = {
-          type = "EF02"; # for grub MBR
-          size = "1M";
+    content.type = "gpt";
+    content.partitions = {
+      MBR = {
+        type = "EF02"; # for grub MBR
+        size = "1M";
+      };
+      ESP = {
+        type = "EF00";
+        size = "1G";
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
         };
-        ESP = {
-          type = "EF00";
-          size = "1G";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-          };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
+      };
+      root = {
+        size = "100%";
+        content = {
+          type = "filesystem";
+          format = "ext4";
+          mountpoint = "/";
         };
       };
     };
