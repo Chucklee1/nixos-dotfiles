697acc2c1c4a30cb1c6ecb2cd68eaf313862ab05
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2024-12-09 02:11:00 -0800

    Update bash.nix

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -8,8 +8,8 @@
           cg = "sudo nix-collect-garbage";
           update-laptop = "sudo nixos-rebuild switch --impure --show-trace --flake ./nixos-dotfiles#laptop";
           update-desktop = "sudo nixos-rebuild switch --impure --show-trace --flake ./nixos-dotfiles#desktop";
-          remote-update-laptop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#laptop";
-          remote-update-desktop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#desktop";
+          remote-update-laptop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#laptop --no-write-lock-file";
+          remote-update-desktop = "sudo nixos-rebuild switch --impure --show-trace --flake github:Chucklee1/nixos-dotfiles#desktop --no-write-lock-file";
         };
       };
     }
