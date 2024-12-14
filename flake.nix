7ee2fd76d3568132aa31a9bd3fcc793fa26e23d9
Chucklee1 <cooperkang4@gamil.com>
2024-12-13 16:49:21 -0800

    it works locally pls work remote

--- flake.nix
+++ flake.nix
@@ -8,10 +8,6 @@
     stylix.url = "github:danth/stylix";
     niri.url = "github:sodiboo/niri-flake";
     grub2-themes.url = "github:vinceliuice/grub2-themes";
-    among-us = {
-      url = "github:Chucklee1/nixos-dotfiles";
-      rev = "main"; # Or the commit/branch you're using
-    };
   };
 
   outputs = {
@@ -21,9 +17,6 @@
   } @ inputs: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
-    supportedSystems = ["aarch64-linux" "i686-linux" "x86_64-linux"];
-    forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-    nixpkgsFor = forAllSystems (system: import inputs.nixpkgs {inherit system;});
     wallpaper = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/Chucklee1/assets/refs/heads/main/elqlyrb492u71.PNG";
       sha256 = "0c16zcn5sfq704hi6s0ia200fjdnn2q5yra9hccpqxzrkf4l1lsi";
@@ -68,21 +61,18 @@
     # -----------------------------------------------------------
     # lazy-insaller script
     # -----------------------------------------------------------
-    package.x86_64-linux.default = self.packages.x86_64-linux.lazy-insaller;
-
-    packages.x86_64-linux.lazy-insaller = let
-      ${name} = "lazy-installer";
+    packages.x86_64-linux.lazy-installer = let
+      name = "lazy-installer";
       script = pkgs.writeShellScriptBin name ''
         DATE=$(ddate +'the %e of %B%, %Y')
         cowsay Hello, world! Today is $DATE.
       '';
-      dependants = with pkgs; [cowsay ddate];
     in
       pkgs.symlinkJoin {
-        name = ${name};
-        paths = [script] ++ dependants;
+        name = name;
+        paths = [script] ++ [pkgs.cowsay pkgs.ddate];
         buildInputs = [pkgs.makeWrapper];
-        postBuild = "wrapProgram $out/bin/${${name}} --prefix PATH : $out/bin";
+        postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
       };
   };
 }
