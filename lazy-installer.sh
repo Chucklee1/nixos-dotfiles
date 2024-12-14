4b70ed2c7a36638ceb5acd0ad2efa42a8b0a872f
Chucklee1 <cooperkang4@gamil.com>
2024-12-13 19:06:20 -0800

    putting on sudo all the command i want privilaged, skull emoji

--- lazy-installer.sh
+++ lazy-installer.sh
@@ -6,44 +6,42 @@ msg-sleep() {
     sleep 2
 }
 
-sudo -i
-
 msg-sleep "partitioning ${DISK}..."
 
-parted $DISK -- mklabel gpt
+sudo parted $DISK -- mklabel gpt
 msg-sleep "created gpt label"
 
-parted $DISK -- --script mkpart ESP fat32 1MB 1G
-parted $DISK -- --script set 1 esp on
-parted "created boot partition"
+sudo parted $DISK -- --script mkpart ESP fat32 1MB 1G
+sudo parted $DISK -- --script set 1 esp on
+sudo parted "created boot partition"
 
-parted $DISK -- --script mkpart root 1G 100%
+sudo parted $DISK -- --script mkpart root 1G 100%
 msg-sleep "created root partition"
 
 
 msg-sleep "formatting ${DISK}..."
 
-mkfs.fat -F32 -n BOOT ${DISK}p1
+sudo mkfs.fat -F32 -n BOOT ${DISK}p1
 msg-sleep "formatted BOOT"
 
-mkfs.ext4 -L NIXOS-ROOT ${DISK}p2
+sudo mkfs.ext4 -L NIXOS-ROOT ${DISK}p2
 msg-sleep "formatted NIXOS-ROOT"
 
 
 msg-sleep "mounting root partition to /mnt"
-mount ${DISK}p2 /mnt
+sudo mount ${DISK}p2 /mnt
 msg-sleep "success"
 
 msg-sleep "creating boot directory"
-mkdir /mnt/boot
+sudo mkdir /mnt/boot
 msg-sleep "success"
 
 msg-sleep "mounting boot partition to /mnt/boot"
-mount ${DISK}p1 /mnt/boot
+sudo mount ${DISK}p1 /mnt/boot
 msg-sleep "success"
 
 msg-sleep "generating hardware config"
 sudo nixos-generate-config --only-hardware --root /mnt > /home/nixos/nixos-dotfiles/modules/machines/desktop.nix
 msg-sleep "installing nixos"
-nixos-install --flake /home/nixos/nixos-dotfiles/#desktop
+sudo nixos-install --flake /home/nixos/nixos-dotfiles/#desktop
 
