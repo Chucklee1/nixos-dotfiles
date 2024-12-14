68d39fd997e3a239127925abe0f2f87ef073044b
Chucklee1 <cooperkang4@gamil.com>
2024-12-13 21:35:48 -0800

    added prism launcher

--- modules/general/games.nix
+++ modules/general/games.nix
@@ -5,4 +5,7 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+  environment.systemPackages = with pkgs; [
+    prism-launcher
+  ];
 }
