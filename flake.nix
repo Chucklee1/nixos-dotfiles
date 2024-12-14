7b01f7bd225a5749d835c6846716095d653778fc
Chucklee1 <cooperkang4@gamil.com>
2024-12-13 16:39:37 -0800

    cmon work

--- flake.nix
+++ flake.nix
@@ -68,7 +68,7 @@
     # -----------------------------------------------------------
     # lazy-insaller script
     # -----------------------------------------------------------
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.lazy-insaller;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.lazy-insaller;
 
     packages.x86_64-linux.lazy-insaller = let
       ${name} = "lazy-installer";
