1c7df36b0834ab65e0ad7c48dfb35fe6f8ec372a
Chucklee1 <cooperkang4@gamil.com>
2024-12-13 18:08:09 -0800

    updated script

--- lazy-installer.sh
+++ lazy-installer.sh
@@ -1,15 +1,16 @@
 DISK="/dev/${1}"
 
 echo "partitioning disk ${DISK}"
-parted $DISK
-mklabel gpt
-mkpart ESP fat32 1MB 1G
-set 1 esp on
-mkpart root ext4 1G 
+parted $DISK -- mklabel gpt
+parted $DISK -- mkpart ESP fat32 1MB 1G
+parted $DISK -- set 1 esp on
+parted $DISK -- mkpart root ext4 1G 
+sleep 2
 
 echo "formatting disk ${DISK}"
 mkfs.fat -F32 -n BOOT ${DISK}p1
 mkfs.ext4 -L NIXOS-ROOT ${DISK}p2
+sleep 2
 
 echo "mounting root partition to /mnt"
 mount /dev/${DISK}p2 /mnt
@@ -17,6 +18,7 @@ echo "creating boot directory"
 mkdir /mnt/boot
 echo "mounting boot partition to /mnt/boot"
 mount /dev/${DISK}p1 /mnt/boot
+sleep 2
 
 echo "cloning repo"
 git clone https://github.com/Chucklee1/nixos-dotfiles
