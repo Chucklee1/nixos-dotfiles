6ee15a2e09504516f40f18585f858a0bd9864e03
Chucklee1 <cooperkang4@gamil.com>
2024-12-14 14:13:07 -0800

    added config options for bspwmrc

--- modules/programs/bspwm.nix
+++ modules/programs/bspwm.nix
@@ -9,22 +9,141 @@
   };
 
   config = lib.mkIf config.bspwm.enable {
+    services.xserver = {
+      enable = true;
+      windowManager.bspwm = {
+        enable = true;
+        configFile = builtins.toFile "bspwmrc" ''
+          pgrep -x sxhkd > /dev/null || sxhkd &
+
+          bspc monitor -d I II III IV V VI VII VIII IX X
+
+          bspc config border_width         2
+          bspc config window_gap          12
+
+          bspc config split_ratio          0.52
+          bspc config borderless_monocle   true
+          bspc config gapless_monocle      true
+
+          bspc rule -a Gimp desktop='^8' state=floating follow=on
+          bspc rule -a Chromium desktop='^2'
+          bspc rule -a mplayer2 state=floating
+          bspc rule -a Kupfer.py focus=on
+          bspc rule -a Screenkey manage=off
+        '';
+
+        sxhkd.configFile = builtins.toFile "sxhkdrc" ''
+          # terminal emulator
+          super + Return
+            kitty
+
+          # program launcher
+          super + @space
+            rofi
+
+          # make sxhkd reload its configuration files:
+          super + Escape
+            pkill -USR1 -x sxhkd
+
+
+          # quit/restart bspwm
+          ctrl + alt + {del,r}
+            bspc {quit,wm -r}
+
+          # close and kill
+          super + {_,shift + }q
+            bspc node -{c,k}
+
+          # alternate between the tiled and monocle layout
+          super + m
+            bspc desktop -l next
+
+          # send the newest marked node to the newest preselected node
+          super + y
+            bspc node newest.marked.local -n newest.!automatic.local
+
+          # swap the current node and the biggest window
+          super + g
+            bspc node -s biggest.window
+
+          # set the window state
+          super + {t,shift + t,s,f}
+            bspc node -t {tiled,pseudo_tiled,floating,fullscreen}
+
+          # set the node flags
+          super + ctrl + {m,x,y,z}
+            bspc node -g {marked,locked,sticky,private}
+
+          # focus the node in the given direction
+          super + {_,shift + }{h,j,k,l}
+            bspc node -{f,s} {west,south,north,east}
+
+          # focus the node for the given path jump
+          super + {p,b,comma,period}
+            bspc node -f @{parent,brother,first,second}
+
+          # focus the next/previous window in the current desktop
+          super + {_,shift + }c
+            bspc node -f {next,prev}.local.!hidden.window
+
+          # focus the next/previous desktop in the current monitor
+          super + bracket{left,right}
+            bspc desktop -f {prev,next}.local
+
+          # focus the last node/desktop
+          super + {grave,Tab}
+            bspc {node,desktop} -f last
+
+          # focus the older or newer node in the focus history
+          super + {o,i}
+            bspc wm -h off; \
+            bspc node {older,newer} -f; \
+            bspc wm -h on
+
+          # focus or send to the given desktop
+          super + {_,shift + }{1-9,0}
+            bspc {desktop -f,node -d} '^{1-9,10}'
+
+          #
+          # preselect
+          #
+
+          # preselect the direction
+          super + ctrl + {h,j,k,l}
+            bspc node -p {west,south,north,east}
+
+          # preselect the ratio
+          super + ctrl + {1-9}
+            bspc node -o 0.{1-9}
+
+          # cancel the preselection for the focused node
+          super + ctrl + space
+            bspc node -p cancel
+
+          # cancel the preselection for the focused desktop
+          super + ctrl + shift + space
+            bspc query -N -d | xargs -I id -n 1 bspc node id -p cancel
+
+          # expand a window by moving one of its side outward
+          super + alt + {h,j,k,l}
+            bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}
+
+          # contract a window by moving one of its side inward
+          super + alt + shift + {h,j,k,l}
+            bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}
+
+          # move a floating window
+          super + {Left,Down,Up,Right}
+            bspc node -v {-20 0,0 20,0 -20,20 0}
+        '';
+      };
+    };
     home-manager.sharedModules = [
       {
         home.packages = with pkgs; [
-          i3
-          i3blocks
           rofi # A more flexible alternative to dmenu
           feh # For setting background images
         ];
-        #programs.i3blocks.enable = true;
-        xsession = {
-          enable = true;
-
-          windowManager.i3 = {
-            enable = true;
-          };
-        };
       }
     ];
   };
