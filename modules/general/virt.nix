7917128e0e2df1bae6babda0cc11bb8418aa2c9e
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-18 12:24:01 -0800

    added virt manager

--- modules/general/virt.nix
+++ modules/general/virt.nix
@@ -7,22 +7,28 @@
   options.virt.enable = lib.mkEnableOption "enables vm utils";
 
   config = lib.mkIf config.virt.enable {
-    programs.dconf.enable = true;
-    virtualisation.libvirtd = {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMF.override {
-              secureBoot = true;
-              tpmSupport = true;
-            })
-            .fd
-          ];
+    programs = {
+      virt-manager.enable = true;
+      dconf.enable = true;
+    };
+    virtualisation = {
+      spiceUSBRedirection.enable = true;
+      libvirtd = {
+        enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = true;
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = [
+              (pkgs.OVMF.override {
+                secureBoot = true;
+                tpmSupport = true;
+              })
+              .fd
+            ];
+          };
         };
       };
     };
