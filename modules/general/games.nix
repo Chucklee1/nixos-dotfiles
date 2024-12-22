0021db9837bcdb76008fb6b554c6bb16da9ab633
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-22 00:23:41 -0800

    added samba and winetricks

--- modules/general/games.nix
+++ modules/general/games.nix
@@ -17,6 +17,8 @@
     environment.variables = {STEAM_EXTRA_COMPAT_TOOLS_PATHS = "~/.steam/root/compatibilitytools.d";};
     environment.systemPackages = with pkgs; [
       wineWowPackages.stagingFull
+      samba
+      winetricks
       protonup-qt
       protontricks
       vulkan-tools
