361db1c8fb4249d3203c14a56c3565d7a7edb9cd
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-22 19:00:24 -0800

    added keybinds

--- modules/configs/nixvim/keymappings.nix
+++ modules/configs/nixvim/keymappings.nix
@@ -2,7 +2,24 @@
   home-manager.sharedModules = [
     {
       programs.nixvim = {
-        keymaps = [];
+        keymaps = [
+  {
+    key = "<leader>e";
+    action = "<cmd>NvimTreeToggle<CR>";
+    options = {
+      silent = true;
+      noremap = true;
+    };
+  }
+  {
+    key = "<leader>f";
+    action = "<cmd>NvimTreeFocus<CR>";
+    options = {
+      silent = true;
+      noremap = true;
+    };
+  }
+];
       };
     }
   ];
