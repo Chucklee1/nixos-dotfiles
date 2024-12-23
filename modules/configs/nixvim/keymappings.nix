cadf7ed824d181a3c8016fa0ec9f199525d1f098
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-22 21:55:09 -0800

    updated desktop config, updated system module, reseting nixvim

--- modules/configs/nixvim/keymappings.nix
+++ modules/configs/nixvim/keymappings.nix
@@ -6,16 +6,26 @@
 	keymaps = [
   {
     mode = ["n"];
-    key = "<leader>e";
+    key = "<leader>f";
     action = "<cmd>NvimTreeToggle<CR>";
     options = {
       silent = true;
       noremap = true;
     };
   }
-  {
-    key = "<leader>f";
-    action = "<cmd>NvimTreeFocus<CR>";
+    {
+    mode = ["n"];
+    key = "<leader>h";
+    action = "<cmd>BufferLineCycleNext<CR>";
+    options = {
+      silent = true;
+      noremap = true;
+    };
+  }
+    {
+    mode = ["n"];
+    key = "<leader>l";
+    action = "<cmd>BufferLineCycleNext<CR>";
     options = {
       silent = true;
       noremap = true;
