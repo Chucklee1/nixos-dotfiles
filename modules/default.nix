3b1cc99dcc7ece81d8ed02e605eba72fe4aacc68
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-22 21:57:29 -0800

    updated default module path

--- modules/default.nix
+++ modules/default.nix
@@ -3,12 +3,9 @@
     ./general/system.nix # main config
     ./general/virt.nix
 
-    ./configs/nixvim/plugins.nix
-    ./configs/nixvim/options.nix
-    ./configs/nixvim/keymappings.nix
-
     ./configs/hyprland.nix
     ./configs/niri.nix
+    ./configs/nixvim.nix
     ./configs/shelli.nix
     ./configs/steam.nix
     ./configs/theme.nix
