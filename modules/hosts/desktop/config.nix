929e28b8201f4a28308522e4c473da99338baf1f
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-24 14:20:07 -0800

    removed the weird module stuff from laptop and desktop modules

--- modules/hosts/desktop/config.nix
+++ modules/hosts/desktop/config.nix
@@ -1,108 +1,105 @@
-_: {
-  desktop.modules = [
-    ({
-      config,
-      lib,
-      pkgs,
-      def,
-      ...
-    }: {
-      imports = [./hardware.nix];
-      # -----------------------------------------------------------
-      # system
-      # -----------------------------------------------------------
-      boot = {
-        initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
-        kernelModules = ["kvm-amd"];
-        supportedFilesystems = ["ntfs"];
-      };
+{
+  config,
+  lib,
+  pkgs,
+  def,
+  ...
+}: {
+  imports = [./hardware.nix];
+  # -----------------------------------------------------------
+  # system
+  # -----------------------------------------------------------
+  boot = {
+    initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+    kernelModules = ["kvm-amd"];
+    supportedFilesystems = ["ntfs"];
+  };
 
-      system.stateVersion = "24.05"; # DO NOT CHANGE
-      networking = {
-        interfaces.enp7s0.useDHCP = lib.mkDefault true;
-        interfaces.wlp6s0.useDHCP = lib.mkDefault true;
-      };
+  system.stateVersion = "24.05"; # DO NOT CHANGE
+  networking = {
+    interfaces.enp7s0.useDHCP = lib.mkDefault true;
+    interfaces.wlp6s0.useDHCP = lib.mkDefault true;
+  };
 
-      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-      services.xserver.videoDrivers = ["nvidia"];
-      hardware.nvidia = {
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
-        open = false;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
-      };
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
-      # -----------------------------------------------------------
-      # packages
-      # -----------------------------------------------------------
-      environment.systemPackages = with pkgs; [
-        # tools/deps
-        nvidia-vaapi-driver
-        zenity
-        wineWowPackages.stagingFull
-        samba
-        winetricks
-        protonup-qt
-        protontricks
-        # hyprland
-        hyprland-protocols
-        hyprshot
-        hypridle
-        hyprlock
-        hyprsunset
-        # games
-        osu-lazer-bin
-        prismlauncher
-      ];
-
-      # -----------------------------------------------------------
-      # programs
-      # -----------------------------------------------------------
-      programs = {
-        hyprland.enable = true;
-        virt-manager.enable = true;
-        dconf.enable = true;
-        gamemode.enable = true;
-        steam = {
-          enable = true;
-          remotePlay.openFirewall = true;
-          dedicatedServer.openFirewall = true;
-          localNetworkGameTransfers.openFirewall = true;
-        };
-      };
-
-      # -----------------------------------------------------------
-      # misc
-      # -----------------------------------------------------------
-      environment.variables = {STEAM_EXTRA_COMPAT_TOOLS_PATHS = "~/.steam/root/compatibilitytools.d";};
-      virtualisation = {
-        spiceUSBRedirection.enable = true;
-        libvirtd = {
-          enable = true;
-          qemu = {
-            package = pkgs.qemu_kvm;
-            runAsRoot = true;
-            swtpm.enable = true;
-            ovmf = {
-              enable = true;
-              packages = [
-                (pkgs.OVMF.override {
-                  secureBoot = true;
-                  tpmSupport = true;
-                })
-                .fd
-              ];
-            };
-          };
-        };
-      };
-    })
+  # -----------------------------------------------------------
+  # packages
+  # -----------------------------------------------------------
+  environment.systemPackages = with pkgs; [
+    # tools/deps
+    nvidia-vaapi-driver
+    zenity
+    wineWowPackages.stagingFull
+    samba
+    winetricks
+    protonup-qt
+    protontricks
+    # hyprland
+    hyprland-protocols
+    hyprshot
+    hypridle
+    hyprlock
+    hyprsunset
+    # games
+    osu-lazer-bin
+    prismlauncher
   ];
+
+  # -----------------------------------------------------------
+  # programs
+  # -----------------------------------------------------------
+  programs = {
+    hyprland.enable = true;
+    virt-manager.enable = true;
+    dconf.enable = true;
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+  };
+
+  # -----------------------------------------------------------
+  # misc
+  # -----------------------------------------------------------
+  environment.variables = {STEAM_EXTRA_COMPAT_TOOLS_PATHS = "~/.steam/root/compatibilitytools.d";};
+  virtualisation = {
+    spiceUSBRedirection.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            })
+            .fd
+          ];
+        };
+      };
+    };
+  };
+
   # -----------------------------------------------------------
   # home manager
   # -----------------------------------------------------------
-  desktop.home-modules = [
-    ({pkgs, ...}: {
+  home-manager.sharedModules = [
+    {
       dconf.settings = {
         "org/virt-manager/virt-manager/connections" = {
           autoconnect = ["qemu:///system"];
@@ -237,6 +234,6 @@ _: {
           ];
         };
       };
-    })
+    }
   ];
 }
