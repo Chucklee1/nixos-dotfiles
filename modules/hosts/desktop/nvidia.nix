a5518ff83d6120d86761833095e860f32ae48e54
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-28 14:48:13 -0800

    updated env vars

--- modules/hosts/desktop/nvidia.nix
+++ modules/hosts/desktop/nvidia.nix
@@ -9,18 +9,11 @@ in {
   nixpkgs.config.nvidia.acceptLicense = true;
   services.xserver.videoDrivers = ["nvidia"];
 
-  environment = {
-    variables = {
-      GBM_BACKEND = "nvidia-drm";
-      LIBVA_DRIVER_NAME = "nvidia";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    };
-    systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
       vulkan-loader
       vulkan-validation-layers
       vulkan-tools
     ];
-  };
 
   hardware = {
     nvidia = {
