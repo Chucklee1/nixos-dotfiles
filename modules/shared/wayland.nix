53a0482349dbff3e526d7ca3edc2fe881ad05e57
Chucklee1 - remote <cooperkang4@gmail.com>
2024-12-31 13:39:21 -0800

    added wayland.nix?

---
+++ modules/shared/wayland.nix
@@ -0,0 +1,45 @@
+{lib, pkgs, ...}:
+{
+  programs = {
+    fuzzel.enable = true;
+    wlogout = {
+      enable = true;
+    };
+    swaylock = {
+      enable = true;
+      package = pkgs.swaylock-effects;
+    };
+  };
+
+  services = {
+    dunst.enable = true;
+    gnome-keyring.enable = true;
+    swayidle = {
+      enable = true;
+    };
+  };
+
+  systemd.user.services.wlstart = {
+    Unit.Description = "wayland startup service";
+    Install.WantedBy = ["default.target"];
+    Service = {
+      ExecCondition = ''
+        if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+          exit 0
+        else
+          exit 1
+        fi
+      '';
+      ExecStart = ''
+        if systemctl --user list-units --type=service --all | grep -q 'waybar.service'; then
+          systemctl --user restart waybar.service
+        fi
+        ${lib.getExe pkgs.networkmanagerapplet}
+        ${lib.getExe pkgs.wlsunset} -T 5200
+        ${lib.getExe pkgs.swaybg} -i ${def.wallpaper} -m fill
+      '';
+      Restart = "always";
+      RestartSec = "2";
+    };
+  };
+}
