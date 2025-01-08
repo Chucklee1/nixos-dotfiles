e45e22c488d3b589258b166aa3ed74ae3d9b4f15
Chucklee1 - remote <cooperkang4@gmail.com>
2025-01-08 00:18:52 -0800

    changes dwm to use dmw submodule

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -179,6 +179,7 @@
     enable = true;
     xkb.layout = def.layout;
     windowManager.dwm.enable = true;
+    windowManager.dwm.package = pkgs.dwm.overrideAttrs {src = ../../assets/dwm}
   };
 
   # audio
