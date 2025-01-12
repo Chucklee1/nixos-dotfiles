f83d1f10e5674d5beb3dcd67beec4059545dc9b9
Chucklee1 - remote <cooperkang4@gmail.com>
2025-01-11 22:30:30 -0800

    removed bin module, added dwm-status and made picom its own service

--- modules/shared/dwm.nix
+++ modules/shared/dwm.nix
@@ -5,74 +5,95 @@
 }: {
   # overrides
   nixpkgs.overlays = [
-    (import ./bin.nix)
     (self: super: {
       dwm = super.dwm.overrideAttrs (oldAttrs: {
         src = /home/goat/dwm;
       });
     })
-    (self: super: {
-      dwmblocks = super.dwmblocks.overrideAttrs (oldAttrs: {
-        src = /home/goat/dwmblocks;
-        nativeBuildInputs = [pkgs.pkg-config];
-        buildInputs =
-          super.dwmblocks.buildInputs
-          ++ builtins.attrValues {inherit (pkgs.xorg) libxcb xcbutil;};
-      });
-    })
   ];
 
-  # X11
-  services.xserver = {
-    enable = true;
-    xkb.layout = def.layout;
-    desktopManager.xterm.enable = false;
-    windowManager.session = [
-      {
-        name = "none+dwm";
-        start = ''
-          export _JAVA_AWT_WM_NONREPARENTING=1
-          dwm &
-          waitPID=$!
+  services = {
+    xserver = {
+      enable = true;
+      xkb.layout = def.layout;
+      desktopManager.xterm.enable = false;
+      windowManager.session = [
+        {
+          name = "none+dwm";
+          start = ''
+            export _JAVA_AWT_WM_NONREPARENTING=1
+            dwm &
+            waitPID=$!
 
-          if xrandr | grep -q "DP-2"; then
-            xrandr --output DP-2 --mode 1920x1080 -r 165.00
-          else
-            echo "DP-2 does not exist"
-          fi
+            if xrandr | grep -q "DP-2"; then
+              xrandr --output DP-2 --mode 1920x1080 -r 165.00
+            else
+              echo "DP-2 does not exist"
+            fi
 
-          feh --bg-scale ${def.wallpaper}
-          redshift -O 5200
-          picom
-          dwmblocks
-        '';
-      }
-    ];
-  };
+            ${lib.getExe pkgs.feh} --bg-scale ${def.wallpaper}
+            ${lib.getExe pkgs.redshift} -O 5200
+          '';
+        }
+      ];
+    };
+    # status bar
+    upower.enable = true;
+    dwm-status = {
+      enable = true;
+      order = ["audio" "backlight" "battery" "cpu_load" "network" "time"];
+      extraConfig = ''
+        separator = "    "
 
-  # system
-  environment = {
-    systemPackages = with pkgs; [
-      dwm
-      dwmblocks
-      dmenu
-      redshift
-      light
-      picom
-      feh
-      xclip
-      nerd-fonts.symbols-only
-      # bin scripts
-      get-volume
-      get-brightness
-      get-battery
-      get-net
-      get-date
-    ];
-    variables = {
-      _JAVA_AWT_WM_NONREPARENTING = 1;
+        [audio]
+        control = "Master"
+        mute = ""
+        template = "{ICO} {VOL}%"
+        icons = []
+
+        [backlight]
+        device = "auto"
+        template = "{ICO} {BL}%"
+        icons = [󱩎, 󱩒, 󱩖]
+
+        [battery]
+        charging = "󱐋"
+        discharging = "󰚦"
+        enable_notifier = true
+        no_battery = "󱉝"
+        notifier_critical = 10
+        notifier_levels = [2, 5, 10, 15, 20]
+        separator = " · "
+        icons = [, , , , ]
+
+        [cpu_load]
+        template = "{CL1} {CL5} {CL15}"
+        update_interval = 20
+
+        [network]
+        no_value = "NA"
+        template = "{LocalIPv4} · {ESSID}"
+
+        [time]
+        format = "%Y-%m-%d %H:%M"
+        update_seconds = false
+      '';
+    };
+    # compositor
+    picom = {
+      enable = true;
+      backend = "egl";
+      vSync = "opengl";
     };
   };
+
+  # packages
+  environment.systemPackages = with pkgs; [
+    dwm
+    dmenu
+    light
+    xclip
+  ];
 
   # desktop portal
   xdg.portal = {
--- modules/shared/shelli.home.nix
+++ modules/shared/shelli.home.nix
@@ -12,7 +12,7 @@ _: {
       settings = {
         scrollback_lines = 2000;
         wheel_scroll_min_lines = 1;
-        window_padding_width = 4;
+        window_padding_width = 0;
         confirm_os_window_close = 0;
         window_border_width = "0px";
         tab_bar_edge = "top";
