c74b4ca175cfb4b656c96978250c2f4139a0807e
Chucklee1 - remote <cooperkang4@gmail.com>
2025-01-20 01:31:01 -0800

    updated waybar

--- modules/niri/waybar.nix
+++ modules/niri/waybar.nix
@@ -1,128 +1,120 @@
-{
+{config, ...}: {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    settings = {
-      position = "top";
-      layer = "top";
-      height = 30;
-      spacing = 4;
+    settings = [
+      {
+        position = "top";
+        layer = "top";
+        height = 20;
+        spacing = 4;
 
-      modules-left = [
-        "niri/workspaces"
-        "niri/window"
-        "custom/media"
-      ];
-      "custom/media" = {
-        escape = true;
-        exec = "$HOME/.config/waybar/mediaplayer.py --player spotify 2> /dev/null";
-        format = "{icon} {text}";
-        format-icons = {
-          default = "🎜";
-          spotify = "";
+        modules-left = [
+          "idle_inhibitor"
+          "cpu"
+          "memory"
+          "niri/window"
+        ];
+
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
+          };
         };
-        max-length = 40;
-        return-type = "json";
-      };
 
-      modules-center = [
-        "clock"
-      ];
-      clock = {format = "{:%H:%M:%S}";};
-
-      modules-right = [
-        "idle_inhibitor"
-        "pulseaudio"
-        "network"
-        "power-profiles-daemon"
-        "cpu"
-        "memory"
-        "backlight"
-        "battery"
-        "tray"
-        "custom/power"
-      ];
-
-      idle_inhibitor = {
-        format = "{icon}";
-        format-icons = {
-          activated = "";
-          deactivated = "";
+        cpu = {
+          format = "{usage}% ";
+          tooltip = false;
         };
-      };
 
-      pulseaudio = {
-        format = "{volume}% {icon} {format_source}";
-        format-bluetooth = "{volume}% {icon} {format_source}";
-        format-icons = {default = ["" "" ""];};
-        format-muted = " {format_source}";
-        format-source = "{volume}% ";
-        format-source-muted = "";
-        on-click = "pavucontrol";
-      };
+        memory = {format = "{}% ";};
 
-      network = {
-        format-alt = "{ifname}: {ipaddr}/{cidr}";
-        format-disconnected = "Disconnected ⚠";
-        format-ethernet = "{ipaddr}/{cidr} ";
-        format-wifi = "{essid} ({signalStrength}%) ";
-        tooltip-format = "{ifname} via {gwaddr} ";
-      };
+        modules-center = ["clock"];
+        clock = {format = "{:%H:%M:%S}";};
 
-      power-profiles-daemon = {
-        format = "{icon}";
-        format-icons = {
-          balanced = "";
-          default = "";
-          performance = "";
-          power-saver = "";
+        modules-right = [
+          "pulseaudio"
+          "network"
+          "backlight"
+          "power-profiles-daemon"
+          "battery"
+          "tray"
+          "custom/power"
+        ];
+
+        pulseaudio = {
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-icons = {default = ["" "" ""];};
+          format-muted = " {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          on-click = "pavucontrol";
         };
-        tooltip = true;
-        tooltip-format = "Power profile: {profile}\nDriver: {driver}";
-      };
 
-      cpu = {
-        format = "{usage}% ";
-        tooltip = false;
-      };
-
-      memory = {format = "{}% ";};
-
-      backlight = {
-        format = "{percent}% {icon}";
-        format-icons = ["" "" "" "" "" "" "" "" ""];
-      };
-
-      battery = {
-        format = "{capacity}% {icon}";
-        format-alt = "{time} {icon}";
-        format-charging = "{capacity}% ";
-        format-full = "{capacity}% {icon}";
-        format-icons = ["" "" "" "" ""];
-        format-plugged = "{capacity}% ";
-        states = {
-          critical = 15;
-          warning = 30;
+        network = {
+          format-alt = "{ifname}: {ipaddr}/{cidr}";
+          format-disconnected = "Disconnected ⚠";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          format-wifi = "{essid} ({signalStrength}%) ";
+          tooltip-format = "{ifname} via {gwaddr} ";
         };
-      };
 
-      tray = {spacing = 10;};
+        backlight = {
+          format = "{percent}% {icon}";
+          format-icons = ["" "" "" "" "" "" "" "" ""];
+        };
 
-      "custom/power" = {
-        format = "⏻ ";
-        on-click = "wlogout";
-      };
-    };
+        power-profiles-daemon = {
+          format = "{icon}";
+          format-icons = {
+            balanced = "";
+            default = "";
+            performance = "";
+            power-saver = "";
+          };
+          tooltip = true;
+          tooltip-format = "Power profile: {profile}\nDriver: {driver}";
+        };
 
-    style = ''
+        battery = {
+          format = "{capacity}% {icon}";
+          format-alt = "{time} {icon}";
+          format-charging = "{capacity}% ";
+          format-full = "{capacity}% {icon}";
+          format-icons = ["" "" "" "" ""];
+          format-plugged = "{capacity}% ";
+          states = {
+            critical = 15;
+            warning = 30;
+          };
+        };
+
+        tray = {spacing = 10;};
+
+        "custom/power" = {
+          format = "⏻ ";
+          on-click = "wlogout";
+        };
+      }
+    ];
+
+    style = let 
+      black = config.scheme.withHashtag.base00;
+      text = config.scheme.withHashtag.base07;
+      background = config.scheme.withHashtag.base01;
+      foreground = config.scheme.withHashtag.base03;
+    in ''
       * {
         font-family: "JetBrainsMono nerd font";
-        font-size: 13px;
+        font-size: 12px;
       }
 
       window#waybar {
-        background-color: rgba(43, 48, 59, 0.5);
-        color: #ffffff;
+        background-color: ${background};
+        color: ${text};
         transition-property: background-color;
         transition-duration: .5s;
       }
@@ -142,31 +134,26 @@
 
       button:hover {
         background: inherit;
-        box-shadow: inset 0 -3px #ffffff;
+        box-shadow: inset 0 -3px ${text};
       }
 
       #workspaces button {
         padding: 0 5px;
         background-color: transparent;
-        color: #ffffff;
+        color: ${text};
       }
 
       #workspaces button:hover {
-        background: rgba(0, 0, 0, 0.2);
+        background: ${black};
       }
 
       #workspaces button.focused {
-        background-color: #64727D;
-        box-shadow: inset 0 -3px #ffffff;
+        background-color: ${foreground};
+        box-shadow: inset 0 -3px ${text};
       }
 
       #workspaces button.urgent {
         background-color: #eb4d4b;
-      }
-
-      #mode {
-        background-color: #64727D;
-        box-shadow: inset 0 -3px #ffffff;
       }
 
       #clock,
@@ -174,7 +161,6 @@
       #cpu,
       #memory,
       #disk,
-      #temperature,
       #backlight,
       #network,
       #pulseaudio,
@@ -183,11 +169,9 @@
       #tray,
       #mode,
       #idle_inhibitor,
-      #scratchpad,
-      #power-profiles-daemon,
-      #mpd {
+      #power-profiles-daemon {
           padding: 0 10px;
-          color: #ffffff;
+          color: ${text};
       }
 
       #window,
@@ -207,7 +191,7 @@
 
       #battery.critical:not(.charging) {
           background-color: #f53c3c;
-          color: #ffffff;
+          color: ${text};
           animation-name: blink;
           animation-duration: 0.5s;
           animation-timing-function: steps(12);
