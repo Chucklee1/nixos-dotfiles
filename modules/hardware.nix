f926e944c0a15168a3321cab0924714e03403dc0
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2025-04-08 05:53:03 -0700

    Update hardware.nix
    removing impermenance

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -47,70 +47,36 @@ in {
   nix.macbook = [
     inputs.disko.nixosModules.default
     {
-      disko.devices = {
-        disk.main = {
-          device = "/dev/sda";
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              boot = {
-                name = "boot";
-                size = "1M";
-                type = "EF02";
-              };
-              esp = {
-                name = "ESP";
-                size = "500M";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
-              };
-              root = {
-                name = "root";
-                size = "100%";
-                content = {
-                  type = "lvm_pv";
-                  vg = "root_vg";
-                };
+    disko.devices = {
+      disk.main = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
-          };
-        };
-        lvm_vg = {
-          root_vg = {
-            type = "lvm_vg";
-            lvs = {
-              root = {
-                size = "100%FREE";
-                content = {
-                  type = "btrfs";
-                  extraArgs = ["-f"];
-
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                    };
-
-                    "/persist" = {
-                      mountOptions = ["subvol=persist" "noatime"];
-                      mountpoint = "/persist";
-                    };
-
-                    "/nix" = {
-                      mountOptions = ["subvol=nix" "noatime"];
-                      mountpoint = "/nix";
-                    };
-                  };
-                };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
         };
       };
+    };
+  }
     }
     ({modulesPath, ...}: {
       imports = [(modulesPath + "/hardware/network/broadcom-43xx.nix")];
@@ -123,67 +89,4 @@ in {
       };
       hardware.cpu.intel.updateMicrocode = true;
     })
-    inputs.impermanence.nixosModules.impermanence
-    ({lib, ...}: {
-      boot.initrd.postDeviceCommands = lib.mkAfter ''
-        mkdir /btrfs_tmp
-        mount /dev/root_vg/root /btrfs_tmp
-        if [[ -e /btrfs_tmp/root ]]; then
-            mkdir -p /btrfs_tmp/old_roots
-            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-        fi
-
-        delete_subvolume_recursively() {
-            IFS=$'\n'
-            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-                delete_subvolume_recursively "/btrfs_tmp/$i"
-            done
-            btrfs subvolume delete "$1"
-        }
-
-        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-            delete_subvolume_recursively "$i"
-        done
-
-        btrfs subvolume create /btrfs_tmp/root
-        umount /btrfs_tmp
-      '';
-
-      fileSystems."/persist".neededForBoot = true;
-      environment.persistence."/persist/system" = {
-        hideMounts = true;
-        directories = [
-          "/var/log"
-          "/var/lib/bluetooth"
-          "/var/lib/nixos"
-          "/var/lib/systemd/coredump"
-          "/etc/NetworkManager/system-connections"
-          "/var/lib/tailscale/tailscaled.state"
-        ];
-      };
-
-      programs.fuse.userAllowOther = true;
-    })
-  ];
-  home.macbook = [
-    inputs.impermanence.nixosModules.home-manager.impermanence
-    {
-      home.persistence."/persist/home" = {
-        allowOther = true;
-        directories = [
-          "nixos-dotfiles"
-          "Navidrome"
-          "Downloads"
-          "Music"
-          "Pictures"
-          "Documents"
-          ".ssh"
-          ".config/chromium"
-          ".cache/chromium"
-          ".local/share/keyrings"
-        ];
-      };
-    }
-  ];
 }
