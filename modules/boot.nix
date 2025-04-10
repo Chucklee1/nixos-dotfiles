64bb7cda5511337e7f1c120933082e32c4eafff4
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2025-04-10 18:49:07 +0000

    Update boot.nix
    man a lot of typos

--- modules/boot.nix
+++ modules/boot.nix
@@ -8,9 +8,9 @@
           efiSupport = true;
           device = "nodev";
         };
-        # display manager
-        services.displayManager.ly.enable = true;
       };
+      # display manager
+      services.displayManager.ly.enable = true;
     }
   ];
 
