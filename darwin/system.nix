bf42c25300d378d81347f447f69484ae14e203d3
Chucklee1 <kermitthefrog@kakao.com>
2025-04-12 09:01:28 +0000

    more settings for darwin

--- darwin/system.nix
+++ darwin/system.nix
@@ -1,8 +1,49 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  system.stateVersion = 6;
+
   nixpkgs = {
     hostPlatform = lib.mkDefault "x86_64-linux";
     config.allowUnfree = true;
   };
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  system.defaults = {
+    dock = {
+      autohide = true;
+      show-process-indicators = false;
+      show-recents = false;
+      static-only = true;
+    };
+    finder = {
+      AppleShowAllExtensions = true;
+      ShowPathbar = true;
+      FXEnableExtensionChangeWarning = false;
+    };
+    NSGlobalDomain = {
+      AppleKeyboardUIMode = 3;
+      "com.apple.keyboard.fnState" = true;
+    };
+  };
+
+  homebrew = {
+    enable = true;
+
+    /*
+      casks = [];
+
+    masApps = {
+      "Drafts" = 1435957248;
+      "Reeder" = 1529448980;
+      "Things" = 904280696;
+      "Timery" = 1425368544;
+    };
+    */
+  };
+
+  #fonts.fonts = [];
 }
