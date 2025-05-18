c3a3dd8b188657c3a476af513de42eb2191ca28b
Chucklee1 <kermitthefrog@kakao.com>
2025-05-18 09:12:47 -0700

    commented out alias

--- modules/software.nix
+++ modules/software.nix
@@ -109,11 +109,13 @@
                   git clone https://github.com/Chucklee1/nixos-dotfiles
                 fi
               '';
-              # tools - ffmpeg
+              /*
+                 tools - ffmpeg
               wav-to-flac = ''
                 for i in *.wav; do ffmpeg -i "$i" -c:a flac "$i%.*.flac"; done
                 for i in *.wav; do rm "$i"; done
               '';
+              */
             };
         };
         oh-my-posh = {
