68fb8c8cdba0cf55504b466ddef76e8da2a3bf7f
Chucklee1 <kermitthefrog@kakao.com>
2025-05-18 09:12:38 -0700

    added back the filesystem function

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -2,18 +2,28 @@
   inputs,
   self,
   ...
-}: {
+}: let
+  # from sodiboos config
+  filesystem = fsType: path: device: options: {
+    fileSystems.${path} =
+      {inherit device fsType;}
+      // (
+        if options == null
+        then {}
+        else {inherit options;}
+      );
+  };
+  #fs.btrfs = filesystem "btrfs";
+  #fs.ntfs = filesystem "ntfs-3g";
+  fs.ext4 = filesystem "ext4";
+  #fs.vfat = filesystem "vfat";
+in {
   nix.global = [{hardware.enableRedistributableFirmware = true;}];
-
   nix.desktop = [
+    (fs.ext4 "ext4" "/media/goat/BLUE_SATA" "/dev/disk/by-uuid/a6ffb4f9-049c-49a1-8b5f-1aca1b8dca08" null)
     inputs.disko.nixosModules.default
     (import "${self}/assets/disko/ext4.nix" {device = "/dev/sda";})
     {
-      fileSystems."/media/goat/BLUE_SATA" = {
-        device = "/dev/disk/by-uuid/a6ffb4f9-049c-49a1-8b5f-1aca1b8dca08";
-        fsType = "ext4";
-      };
-
       boot = {
         initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
         kernelModules = ["kvm-amd"];
@@ -34,7 +44,7 @@
       boot = {
         initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
         # wl and following 2 lines are a fix to broadcom 43 wifi drivers issues
-        kernelModules = ["kvm-intel" "wl"];
+        kernelModules = ["kvm-intel" "wl" "iwlwifi"];
         extraModulePackages = [config.boot.kernelPackages.broadcom_sta];
         blacklistedKernelModules = lib.mkForce ["b43" "bcma"];
         supportedFilesystems = ["ntfs" "btrfs" "apfs"];
