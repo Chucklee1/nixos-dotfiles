e2ea753f80f48a0da20fe4692f649bf792f51aba
Chucklee1 <kermitthefrog@kakao.com>
2025-05-30 17:28:05 -0700

    added keymaps from nixvim config examples

--- nixvim/keymaps.nix
+++ nixvim/keymaps.nix
@@ -1,30 +1,117 @@
-{lib, ...}: {
+{
   globals.mapleader = " ";
   globals.maplocalleader = " ";
   keymaps = let
-    mkNormalKeyMaps = list: let
-      Elem = int: lib.elemAt (lib.splitString " : " list) int;
-    in {
-      mode = "n";
-      action = "<cmd>${Elem 0}<cr>";
-      key = "${Elem 1}";
-    };
+    mkKeymaps = mode: type: list:
+      map (sublist: {
+        inherit mode;
+        key = builtins.elemAt sublist 1;
+        action = (
+          if type == "wrap"
+          then "<cmd>${builtins.elemAt sublist 0}<CR>"
+          else if type == "raw"
+          then {__raw = builtins.elemAt sublist 0;}
+          else {}
+        );
+      })
+      list;
   in
-    map mkNormalKeyMaps [
-      # misc ui
-      "Oil : <leader>e"
-      "LazyGit : <leader>gg"
+    builtins.concatLists [
+      (mkKeymaps "n" "wrap"
+        [
+          # misc ui
+          ["Oil" "<leader>e"]
+          ["LazyGit" "<leadecope find_files>gcope git_commits"]
 
-      # Telescope bindings
-      "Telescope live_grep : <leader>fn"
-      "Telescope find_files : <leader>f"
-      "Telescope git_commits : <leader>fg"
-      "Telescope oldfiles : <leader>fo"
+          # clear search results
+          ["noh" "<esc>"]
 
-      # Bufferline bindings
-      "BufferLineCycleNext : <S-l>"
-      "BufferLineCyclePrev : <S-h>"
-      "bd : <leader>c"
-      "bd! : <leader-S-c>"
+          # Telescope binding
+          ["Telescope live_grep" "<leader>fn"]
+          ["Telescope find_files" "<leader>f"]
+          ["Telescope git_commits" "<leader>fg"]
+          ["Telescope oldfiles" "<leader>fo"]
+
+          # Bufferline bindings
+          ["BufferLineCycleNext" "<S-l>"]
+          ["BufferLineCyclePrev" "<S-h>"]
+
+          # force it
+          ["w!" "<leader>W"]
+          ["q!" "<leader>Q"]
+          ["bd" "<leader>c"]
+          ["bd!" "<leader>C"]
+        ])
+
+      (mkKeymaps "v" "wrap" [
+        # beter indenting
+        ["<gv" "<S-Tab>"]
+        ["<gv" "<"]
+        [">gv" "<Tab>"]
+        [">gv" ">"]
+      ])
+      # toggles
+      (mkKeymaps "n" "raw" [
+        [
+          ''                      
+            function ()
+              vim.b.disable_diagnostics = not vim.b.disable_diagnostics
+              if vim.b.disable_diagnostics then
+                vim.diagnostic.disable(0)
+              else
+                vim.diagnostic.enable(0)
+              end
+            end''
+          "<leader>ud"
+        ]
+        [
+          ''
+            function ()
+              vim.g.disable_diagnostics = not vim.g.disable_diagnostics
+              if vim.g.disable_diagnostics then
+                vim.diagnostic.disable()
+              else
+                vim.diagnostic.enable()
+              end
+            end''
+          "<leader>uD"
+        ]
+
+        [
+          ''
+            function ()
+              -- vim.g.disable_autoformat = not vim.g.disable_autoformat
+              vim.cmd('FormatToggle!')
+            end''
+          "<leader>uf"
+        ]
+
+        [
+          ''
+            function ()
+              -- vim.g.disable_autoformat = not vim.g.disable_autoformat
+              vim.cmd('FormatToggle')
+            end''
+          "<leader>uF"
+        ]
+
+        [
+          ''
+            function ()
+              if vim.g.spell_enabled then vim.cmd('setlocal nospell') end
+              if not vim.g.spell_enabled then vim.cmd('setlocal spell') end
+              vim.g.spell_enabled = not vim.g.spell_enabled
+            end''
+          "<leader>uS"
+        ]
+
+        [
+          ''
+            function ()
+              vim.wo.wrap = not vim.wo.wrap
+            end''
+          "<leader>uw"
+        ]
+      ])
     ];
 }
