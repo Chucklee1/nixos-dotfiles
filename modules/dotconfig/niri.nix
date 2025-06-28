e65ba0fd5a13a7b8f81d3c6127607cc23ce94562
Chucklee1 <kermitthefrog@kakao.com>
2025-06-28 03:59:17 -0700

    moved env vars all to niri

--- modules/dotconfig/niri.nix
+++ modules/dotconfig/niri.nix
@@ -1,4 +1,19 @@
-{
+let
+  variables = {
+    XDG_CURRENT_DESKTOP = "niri";
+    XDG_SESSION_DESKTOP = "niri";
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    DISPLAY = ":0";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    SDL_VIDEODRIVER = "x11";
+    GDK_BACKEND = "wayland,x11";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+  };
+in {
+  nix.desktop = [{environment = {inherit variables;};}];
   home.desktop = [
     ({
       lib,
@@ -24,7 +39,7 @@
         prefer-no-csd = true;
         hotkey-overlay.skip-at-startup = true;
         screenshot-path = "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png";
-
+        environment = variables;
         spawn-at-startup = let
           get = pkg: lib.getExe pkgs.${pkg};
         in [
--- modules/wayland.nix
+++ modules/wayland.nix
@@ -12,34 +12,19 @@
         package = pkgs.niri-unstable;
       };
 
-      environment = {
-        variables = {
-          XDG_CURRENT_DESKTOP = "niri";
-          XDG_SESSION_DESKTOP = "niri";
-          NIXOS_OZONE_WL = "1";
-          MOZ_ENABLE_WAYLAND = "1";
-          DISPLAY = ":0";
-          _JAVA_AWT_WM_NONREPARENTING = "1";
-          SDL_VIDEODRIVER = "x11";
-          GDK_BACKEND = "wayland,x11";
-          QT_QPA_PLATFORM = "wayland;xcb";
-          QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-          QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-        };
-        systemPackages = with pkgs; [
-          egl-wayland
-          qt5.qtwayland
-          qt6.qtwayland
-          brightnessctl
-          wev
-          wmenu
-          swaynotificationcenter
-          xwayland
-          xwayland-run
-          wl-color-picker
-          wl-clipboard
-        ];
-      };
+      environment.systemPackages = with pkgs; [
+        egl-wayland
+        qt5.qtwayland
+        qt6.qtwayland
+        brightnessctl
+        wev
+        wmenu
+        swaynotificationcenter
+        xwayland
+        xwayland-run
+        wl-color-picker
+        wl-clipboard
+      ];
 
       # polkit n portals
       security.polkit.enable = true;
