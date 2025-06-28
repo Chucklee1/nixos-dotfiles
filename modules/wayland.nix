a5ebe66d6a43d393795ea3c67eaff76ddbfb44a1
Chucklee1 <kermitthefrog@kakao.com>
2025-06-27 21:40:13 -0700

    moved let vars to env vars, added swaynotificationcenter

--- modules/wayland.nix
+++ modules/wayland.nix
@@ -1,18 +1,4 @@
-{inputs, ...}: let
-  environmentVariables = {
-    XDG_CURRENT_DESKTOP = "niri";
-    XDG_SESSION_DESKTOP = "niri";
-    NIXOS_OZONE_WL = "1";
-    MOZ_ENABLE_WAYLAND = "1";
-    DISPLAY = ":0";
-    _JAVA_AWT_WM_NONREPARENTING = "1";
-    SDL_VIDEODRIVER = "x11";
-    GDK_BACKEND = "wayland,x11";
-    QT_QPA_PLATFORM = "wayland;xcb";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-  };
-in {
+{inputs, ...}: {
   nix.desktop = [
     inputs.niri.nixosModules.niri
     ({pkgs, ...}: {
@@ -27,7 +13,19 @@ in {
       };
 
       environment = {
-        variables = environmentVariables;
+        variables = {
+          XDG_CURRENT_DESKTOP = "niri";
+          XDG_SESSION_DESKTOP = "niri";
+          NIXOS_OZONE_WL = "1";
+          MOZ_ENABLE_WAYLAND = "1";
+          DISPLAY = ":0";
+          _JAVA_AWT_WM_NONREPARENTING = "1";
+          SDL_VIDEODRIVER = "x11";
+          GDK_BACKEND = "wayland,x11";
+          QT_QPA_PLATFORM = "wayland;xcb";
+          QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+          QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+        };
         systemPackages = with pkgs; [
           egl-wayland
           qt5.qtwayland
@@ -35,6 +33,7 @@ in {
           brightnessctl
           wev
           wmenu
+          swaynotificationcenter
           xwayland
           xwayland-run
           wl-color-picker
