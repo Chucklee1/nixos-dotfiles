ed2b91e16efa15bcb69b052e87340b7571fa4643
Chucklee1 <kermitthefrog@kakao.com>
2025-07-20 02:25:05 -0700

    updated make.sh to actually do something

--- assets/make.sh
+++ assets/make.sh
@@ -1,18 +1,23 @@
-build() {
-  echo ""
-}
+REPO="$(pwd)/nixos-dotfiles"
 
 install() {
-  echo ""
+    sudo nixos-install --root /mnt --flake "$REPO"
+    read -pr "Before entering chroot to set user password, input username: " username
+    sudo nixos-enter --root /mnt -c "passwd $username"
 }
 
-update() {
-  echo ""
+format() {
+    sudo nix --experimental-features "nix-command flakes" run \
+        github:nix-community/disko -- \
+        --mode disko "$REPO/assets/disko/$1.nix" \
+        --arg device $2
+
+    sudo nixos-generate-config --no-filesystems --show-hardware-config --root /mnt
 }
 
 if declare -f "$1" >/dev/null; then
-  "$@"
+    "$@"
 else
-  echo "Function '$1' not found."
-  exit 1
+    echo "Function '$1' not found."
+    exit 1
 fi
