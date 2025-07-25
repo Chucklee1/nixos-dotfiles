3ecc5d870799fbb4d14bc99425b3c3f1f1aa88bb
Chucklee1 <kermitthefrog@kakao.com>
2025-07-25 15:17:06 -0700

    updated make script

--- assets/make.sh
+++ assets/make.sh
@@ -1,67 +1,75 @@
 # variables
-REPO="$(pwd)/nixos-dotfiles"
+REPO="$HOME/nixos-dotfiles"
+if [ ! -d "$REPO" ]; then
+    REPO="github:Chucklee1/nixos-dotfiles"
+fi
+NIX_CMD="nix --experimental-features 'nix-command flakes'"
+NIX_RUN="$NIX_RUN run"
 
-NL="\e[0m\n" # reset + newline
-ERROR="\e[1;31m" # bold red
-WARN="\e[1;33m" # bold yellow
-INFO="\e[3;33m" # italic yellow
-SUCS="\e[3;32m" # bold green
-
+# print helpers
+msg() {
+    case "$1" in
+    head) printf "\e[1;4m%s\e[0m\n" "$2" ;;
+    opt) printf "  %s\n" "$2" ;;
+    subopt) printf "    %s\n" "$2" ;;
+    *) echo "$2" ;;
+    esac
+}
 layouts() {
     for i in "$REPO"/assets/disko/*; do
         file=${i##*/}
-        echo "${file%.*}"
+        printf "%s " "${file%.*}"
     done
 }
-pipe() {
-    printf '%*s\n' 80 '' | tr ' ' '-'
+
+# logic functions - in system
+rebuild() {
+    local FLAGS="--impure --show-trace"
+    sudo nixos-rebuild switch "$FLAGS" --flake "$REPO#$1"
 }
-# Function to display script usage
-usage() {
-    echo "Usage: $0 [OPTIONS]"
-    pipe
-    echo "Options:"
-    echo " -h, --help         Display this help message"
-    printf %s " -d, --device  partition target in format '\"/dev/sdx\"'"
-    echo " -l, --layout       disk layout type (defaults to ext4)"
-    pipe
-    printf "%s\n%s\n%s\n" \
-        "Current disk layouts:" \
-        "$(layouts)" \
-        "check repo-root/assets/disko for disk-layout definitions"
-    pipe
+update() {
+    "$NIX_CMD" flake update "$REPO"
 }
 
-# logic functions
-install() {
-    sudo nixos-install --root /mnt --flake "$REPO"
-    read -pr "Do you wish to chroot to set a user password (Y/N)? " choice
-    if $choice != 'Y' || 'y'; then
-        printf "${SUCS}%s${NL}" "Setup complete! Exiting script..."
-        exit 0
-    else
-        read -pr "Enter username: " username
-        sudo nixos-enter --root /mnt -c "passwd $username"
-    fi
-}
-
+# logic function - liveboot
 format() {
-    printf "${WARN}%s${NL}${INFO}%s${NL}${WARN}%s${NL}" \
-        "WARNING: This will wipe your specified device" \
-        "this means ALL DATA WILL BE LOST UPON FORMAT" \
-        "do you wish to continue? (YES/abort)"
-
-    if $choice != 'Y' || 'y'; then
-        printf "${SUCS}%s${NL}" "Setup complete! Exiting script..."
-        exit 0
-    else
-        read -pr "Enter username: " username
-        sudo nixos-enter --root /mnt -c "passwd $username"
-    fi
-    sudo nix --experimental-features "nix-command flakes" run \
-        github:nix-community/disko -- \
-        --mode disko "$REPO/assets/disko/$1.nix" \
-        --arg device $2
-
-    sudo nixos-generate-config --no-filesystems --show-hardware-config --root /mnt
+    local FLAGS="--mode disko $REPO/disko/$1.nix --arg device $DEVICE"
+    sudo "$NIX_RUN" github:nix-community/disko -- "$FLAGS"
 }
+print-hardware() {
+    local ROOT="/mnt"
+    local FLAGS="--show-hardware --root $ROOT"
+    sudo nixos-generate-config "$FLAGS"
+}
+install() {
+    local ROOT="/mnt"
+    local FLAGS="--root $ROOT --flake $REPO#$1 --impure --show-trace"
+    sudo nixos-install "$FLAGS"
+}
+
+# ---- main script ----
+
+case "$1" in
+rebuild) rebuild "$@" ;;
+update) update ;;
+format) format "$@" ;;
+install) install "$@" ;;
+print-cfg) print-cfg "$@" ;;
+-h | --help | *)
+    msg head "Usage:"
+    msg opt "$0 [CMD] [OPTIONS]"
+    msg head "Options:"
+    msg opt "-h, --help                 - Displays this message"
+    msg head "Commands:"
+    msg opt "rebuild <profile>          - Rebuild profile config"
+    msg opt "update                     - Update flake inputs"
+    msg opt "format <layout> <device>   - format disk, see below for more info..."
+    msg opt "print-hardware             - outputs hardware configuration to the screen"
+    msg opt "install <profile>          - installs given profile to system on /mnt"
+    msg head "commands - format:"
+    msg opt "<device>:                  - partition target in format '\"/dev/sdx\"'"
+    msg opt "<layout>:                  - disk layout type, choose from: $(layouts)"
+    msg opt "check '$REPO/assets/disko' for disk-layout definitions"
+    exit 0
+    ;;
+esac
