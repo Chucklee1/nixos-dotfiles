1617cf76e57fcc203c9a6e6211139019cf92c4b3
Chucklee1 <kermitthefrog@kakao.com>
2025-08-16 21:56:25 -0700

    added btrfs disko template

---
+++ assets/disko/btrfs.nix
@@ -0,0 +1,44 @@
+{device ? throw "error: proper argument is --arg device /dev/diskName (with single then double wrapped qoutes for device arg /dev/diskName)", ...}: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        inherit device;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              end = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                subvolumes = {
+                  "/rootfs" = {
+                    mountpoint = "/";
+                    mountOptions = ["compress=zstd"];
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
