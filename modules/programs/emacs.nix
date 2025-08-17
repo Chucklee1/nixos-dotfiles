eb30529149c5e310740693937943f2404864c234
Chucklee1 <kermitthefrog@kakao.com>
2025-08-17 09:23:35 -0700

    updated emacs with evil mode

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -1,6 +1,20 @@
 {
   emacs = {
     nix = [{services.emacs.enable = true;}];
-    home = [{programs.emacs.enable = true;}];
+    home = [
+      {
+        programs.emacs = {
+          enable = true;
+          extraPackages = epkgs: [epkgs.evil];
+          extraConfig =
+            #lisp
+            ''
+              ;; Enable Evil
+              (require 'evil)
+              (evil-mode 1)
+            '';
+        };
+      }
+    ];
   };
 }
