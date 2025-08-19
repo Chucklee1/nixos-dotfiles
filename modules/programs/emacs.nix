3a6461493da6a037e4b8e6d39db0ce9c21b06b93
Chucklee1 <kermitthefrog@kakao.com>
2025-08-18 23:12:51 -0700

    upated emacs cfg

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -1,11 +1,44 @@
-{
+{self, ...}: {
   emacs = {
-    nix = [{services.emacs.enable = true;}];
+    nix = [
+      # overlay
+      ({pkgs, ...}: {
+        nixpkgs.overlays = [
+          (import self.inputs.emacs-overlay)
+          (self: super: {
+            emacs-plus = pkgs.emacs-pgtk.overrideAttrs (old: {
+              #patches =
+              #(old.patches or [])
+              #++ [
+              ## Fix OS window role (yabai support)
+              #(super.fetchpatch {
+              #url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+              #sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+              #})
+              #
+              ## Rounded undecorated frame
+              #(super.fetchpatch {
+              #url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+              #sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+              #})
+              #
+              ## Dark/light mode awareness
+              #(super.fetchpatch {
+              #url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
+              #sha256 = "sha256-oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8=";
+              #})
+              #];
+            });
+          })
+        ];
+      })
+      {services.emacs.enable = true;}
+    ];
     home = [
       ({pkgs, ...}: {
         programs.emacs = {
           enable = true;
-          package = pkgs.emacsMacport;
+          package = pkgs.emacs-plus;
           extraPackages = epkgs:
             with pkgs; [
               # vim clone
