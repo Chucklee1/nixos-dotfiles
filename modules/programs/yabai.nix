ea3f43bd0e4ed02063db24826d367e3b95d55d1b
Chucklee1 <kermitthefrog@kakao.com>
2025-08-18 23:12:56 -0700

    removed spacebar

--- modules/programs/yabai.nix
+++ modules/programs/yabai.nix
@@ -1,10 +1,6 @@
 {
   macbook.nix = [
-    ({
-      config,
-      pkgs,
-      ...
-    }:
+    ({config, ...}:
       with config.lib.stylix.colors; let
         bar_height = 26;
       in {
@@ -26,53 +22,6 @@
             auto_balance = "off";
           };
         };
-        services.spacebar = {
-          enable = true;
-          package = pkgs.spacebar;
-          config = {
-            # general
-            position = "top";
-            display = "main";
-            height = bar_height;
-            title = "on";
-            spaces = "on";
-            # clock
-            clock = "on";
-            clock_icon = "";
-            clock_format = ''""%Y-%m-%d | %H:%M:%S""'';
-            # power
-            power = "on";
-            power_icon_strip = " ";
-            # dnd
-            dnd_icon = "";
-            # spaces
-            space_icon = "•";
-            space_icon_strip = "1 2 3 4 5 6 7 8 9 10";
-            spaces_for_all_displays = "on";
-            # right shell
-            right_shell = "on";
-            right_shell_icon = "";
-            right_shell_command = "whoami";
-            # padding
-            padding_left = 0;
-            padding_right = 0;
-            spacing_left = 0;
-            spacing_right = 0;
-            # font
-            text_font = ''"JetBrainsMono Nerd Font:Regular:12.0"'';
-            icon_font = ''"Font Awesome 5 Free:Solid:12.0"'';
-            # color
-            background_color = "0xff${base00}";
-            foreground_color = "0xff${base03}";
-            power_icon_color = "0xff${base0A}";
-            battery_icon_color = "0xff${base0A}";
-            dnd_icon_color = "0xff${base03}";
-            clock_icon_color = "0xff${base03}";
-            space_icon_color = "0xff${base07}";
-            space_icon_color_secondary = "0xff${base0C}";
-            space_icon_color_tertiary = "0xff${base0A}";
-          };
-        };
       })
   ];
   macbook.home = [
@@ -88,5 +37,10 @@
           };
         };
       })
+    ({...}: {
+      programs.sketchybar = {
+        configType = "lua";
+      };
+    })
   ];
 }
