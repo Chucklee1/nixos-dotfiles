e6a785f42757b72440643ca3dfca09bd424eb874
Chucklee1 <kermitthefrog@kakao.com>
2025-08-18 23:12:46 -0700

    updated btrfs layout, added script for arch install

---
+++ assets/notes/arch.sh
@@ -0,0 +1,40 @@
+# NOTE: this is just a lazy instruction script for
+# my desktop, not for normal usage, just ignore this
+
+USERNAME="goat"
+HOSTNAME="$USERNAME-archtop"
+
+# in liveboot
+pacman -S \
+    base base-devel \
+    linux linux-firmware \
+    efibootmgr grub \
+    btrfs-progs grub-btrfs inotify-tools \
+    networkmanager \
+    sudo man openssh git \
+    pacman-contrib reflector \
+    amd-ucode nvidia \
+    pipewire pipewire-pulse pipewire-alsa pipewire-jack \
+    neovim zsh
+
+genfstab -U /mnt >>/mnt/etc/fstab
+
+# chroot
+arch-chroot /mnt
+
+ln -sf /usr/share/ZONE/CITY /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
+local-gen
+echo "en_US.UTF-8" >>/etc/locale.conf
+
+echo "$HOSTNAME" >>/etc/hostname
+printf "%s\n%s\n%s" "127.0.0.1 localhost" "::1 localhost" "127.0.1.1 $HOSTNAME" >>/etc/hosts
+
+useradd -mG wheel "$USERNAME"
+# remember: run passwd and passwd user after
+# also comment out %wheel ALL=(ALL:ALL) ALL
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH
+grub-mkconfig -o /boot/grub/grub.cfg
