10e6907ebcbd51eb4357f1ec03b529680239b868
Chucklee1 <kermitthefrog@kakao.com>
2025-08-19 21:22:53 -0700

    updated emacs & yabai

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -43,6 +43,13 @@
             with pkgs; [
               # vim clone
               epkgs.evil
+              epkgs.evil-collection
+              epkgs.doom-modeline
+              epkgs.nerd-icons
+              epkgs.nerd-icons-dired
+              epkgs.nerd-icons-ibuffer
+              epkgs.org
+              epkgs.eat
               epkgs.auctex
               # lsps/other-related-ish
               nixd
@@ -55,17 +62,139 @@
           extraConfig =
             #lisp
             ''
-              ;; Enable Evil
-              (require 'evil)
-              (evil-mode 1)
+              (setq use-package-always-ensure nil) ;; let nix handle pkgs
+              (setq gc-cons-threshold (* 50 1000 1000)) ;; reduce frequency of garbage collection
 
-              ;; setup latex
-              (setq TeX-source-correlate-method 'synctex)
-              (setq TeX-source-correlate-start-server t)
+              (use-package emacs
+                :custom
+                (menu-bar-mode nil)         ;; Disable the menu bar
+                (scroll-bar-mode nil)       ;; Disable the scroll bar
+                (tool-bar-mode nil)         ;; Disable the tool bar
+                (inhibit-startup-screen t)  ;; Disable welcome screen
 
-              (use-package company
+                (delete-selection-mode t)   ;; Select text and delete it by typing.
+                (electric-indent-mode nil)  ;; Turn off the weird indenting that Emacs does by default.
+                (electric-pair-mode t)      ;; Turns on automatic parens pairing
+
+                (blink-cursor-mode nil)     ;; Don't blink cursor
+                (global-auto-revert-mode t) ;; Automatically reload file and show changes if the file has changed
+
+                (display-line-numbers-type 'relative) ;; Relative line numbers
+                (global-display-line-numbers-mode t)  ;; Display line numbers
+
+                (mouse-wheel-progressive-speed nil) ;; Disable progressive speed when scrolling
+                (scroll-conservatively 10) ;; Smooth scrolling
+                (scroll-margin 8)
+
+                (tab-width 4)
+
+                (make-backup-files nil) ;; Stop creating ~ backup files
+                (auto-save-default nil) ;; Stop creating # auto save files
+                :hook
+                (prog-mode . (lambda () (hs-minor-mode t))) ;; Enable folding hide/show globally
                 :config
-                (global-company-mode 1))
+                ;; Move customization variables to a separate file and load it, avoid filling up init.el with unnecessary variables
+                (setq custom-file (locate-user-emacs-file "custom-vars.el"))
+                (load custom-file 'noerror 'nomessage)
+                :bind (
+                       ([escape] . keyboard-escape-quit) ;; Makes Escape quit prompts (Minibuffer Escape)
+                       ;; Zooming In/Out
+                       ("C-+" . text-scale-increase)
+                       ("C--" . text-scale-decrease)
+                       ("<C-wheel-up>" . text-scale-increase)
+                       ("<C-wheel-down>" . text-scale-decrease)
+                       )
+                )
+
+                (use-package evil
+                  :init
+                  (evil-mode)
+                  :config
+                  (evil-set-initial-state 'eat-mode 'insert) ;; Set initial state in eat terminal to insert mode
+                  :custom
+                  (evil-want-keybinding nil)    ;; Disable evil bindings in other modes (It's not consistent and not good)
+                  (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
+                  (evil-want-C-i-jump nil)      ;; Disables C-i jump
+                  (evil-undo-system 'undo-redo) ;; C-r to redo
+                  ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
+                  :bind (:map evil-motion-state-map
+                              ("SPC" . nil)
+                              ("RET" . nil)
+                              ("TAB" . nil)))
+                (use-package evil-collection
+                  :after evil
+                  :config
+                  ;; Setting where to use evil-collection
+                  (setq evil-collection-mode-list '(dired ibuffer magit corfu vertico consult info))
+                  (evil-collection-init))
+
+                ;; transparency
+                (add-to-list 'default-frame-alist '(alpha-background . 90))
+
+                ;; font
+                (set-face-attribute 'default nil
+                              :font "JetBrains Mono"
+                              :height 120
+                              :weight 'medium)
+                (add-to-list 'default-frame-alist '(font . "JetBrains Mono"))
+                (setq-default line-spacing 0.12)
+
+                ;; modeline
+                (use-package doom-modeline
+                  :custom
+                  (doom-modeline-height 25) ;; Set modeline height
+                  :hook (after-init . doom-modeline-mode))
+
+                ;; nerd fonts
+                (use-package nerd-icons
+                  :if (display-graphic-p))
+
+                (use-package nerd-icons-dired
+                  :hook (dired-mode . (lambda () (nerd-icons-dired-mode t))))
+
+                (use-package nerd-icons-ibuffer
+                  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
+
+                ;; org
+                (use-package org
+                  :ensure nil
+                  :custom
+                  (org-edit-src-content-indentation 4) ;; Set src block automatic indent to 4 instead of 2.
+                  (org-return-follows-link t)   ;; Sets RETURN key in org-mode to follow links
+                  :hook
+                  (org-mode . org-indent-mode) ;; Indent text
+                  ;; The following prevents <> from auto-pairing when electric-pair-mode is on.
+                  ;; Otherwise, org-tempo is broken when you try to <s TAB...
+                  ;;(org-mode . (lambda ()
+                  ;;              (setq-local electric-pair-inhibit-predicate
+                  ;;                          `(lambda (c)
+                  ;;                             (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+                  )
+
+                (use-package eat
+                  :hook ('eshell-load-hook #'eat-eshell-mode))
+
+                ;; eglot
+                (use-package eglot
+                  :ensure nil ;; Don't install eglot because it's now built-in
+                  :hook ((c-mode c++-mode ;; Autostart lsp servers for a given mode
+                                 lua-mode) ;; Lua-mode needs to be installed
+                         . eglot-ensure)
+                  :custom
+                  ;; Good default
+                  (eglot-events-buffer-size 0) ;; No event buffers (LSP server logs)
+                  (eglot-autoshutdown t);; Shutdown unused servers.
+                  (eglot-report-progress nil) ;; Disable LSP server logs (Don't show lsp messages at the bottom, java)
+                  ;; Manual lsp servers
+                  ;;:config
+                  ;;(add-to-list 'eglot-server-programs
+                  ;;             `(lua-mode . ("PATH_TO_THE_LSP_FOLDER/bin/lua-language-server" "-lsp"))) ;; Adds our lua lsp server to eglot's server list
+                  )
+
+                ;; Make gc pauses faster by decreasing the threshold.
+                (setq gc-cons-threshold (* 2 1000 1000))
+                ;; Increase the amount of data which Emacs reads from the process
+                (setq read-process-output-max (* 1024 1024)) ;; 1mb
             '';
         };
       })
--- modules/programs/yabai.nix
+++ modules/programs/yabai.nix
@@ -1,4 +1,4 @@
-{
+{self, ...}: {
   macbook.nix = [
     ({config, ...}:
       with config.lib.stylix.colors; let
@@ -25,73 +25,8 @@
       })
   ];
   macbook.home = [
-    ({
-      config,
-      pkgs,
-      ...
-    }:
+    ({config, ...}:
       with config.lib.stylix.colors; {
-        home.packages = [
-          (pkgs.writeShellScriptBin "sketchy-front_app" ''
-            title=$(yabai -m query --windows --window | jq -r '.title')
-            app=$(yabai -m query --windows --window | jq -r '.app')
-
-            if [ -n "$title" ] && [ "$title" != "null" ]; then
-              echo "  $title"
-            else
-              echo " $app"
-            fi
-          '')
-
-          (pkgs.writeShellScriptBin "sketchy-keyboard" ''
-            layout=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | \
-              grep -A1 'KeyboardLayout Name' | awk -F '"' '/KeyboardLayout Name/{getline; print $2}')
-
-            if [ -z "$layout" ]; then
-              layout="ENG"
-            fi
-
-            echo "⌨ $layout"
-          '')
-
-          (pkgs.writeShellScriptBin "sketchy-volume" ''
-            volume=$(osascript -e "output volume of (get volume settings)")
-            muted=$(osascript -e "output muted of (get volume settings)")
-
-            if [ "$muted" = "true" ]; then
-              echo "🔇 $volume%"
-            else
-              echo "🔊 $volume%"
-            fi
-          '')
-
-          (pkgs.writeShellScriptBin "sketchy-network" ''
-            ssid=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I \
-              | awk -F': ' '/ SSID/ {print $2}')
-
-            if [ -n "$ssid" ]; then
-              echo "📡 $ssid"
-            else
-              echo "⚠️ No Wi-Fi"
-            fi
-          '')
-
-          (pkgs.writeShellScriptBin "sketchy-backlight" ''
-            brightness=$(brightness -l | grep "brightness" | awk '{print int($NF*100)}')
-            echo "☀️ $brightness%"
-          '')
-
-          (pkgs.writeShellScriptBin "sketchy-battery" ''
-            percent=$(pmset -g batt | grep -Eo "[0-9]+%" | cut -d% -f1)
-            status=$(pmset -g batt | grep 'Battery Power')
-
-            if [[ $status == *"AC Power"* ]]; then
-              echo "⚡ $percent%"
-            else
-              echo "🔋 $percent%"
-            fi
-          '')
-        ];
         services.jankyborders = {
           enable = true;
           settings = {
@@ -101,107 +36,9 @@
             inactive_color = ''0x00${base0D}'';
           };
         };
+      home.file.".config/sketchybar".source = "${self}/assets/sketchybar";
         programs.sketchybar = {
           enable = true;
-          configType = "lua";
-          config = ''
-            -- ~/.config/sketchybar/sketchybarrc.lua
-
-            local sbar = require("sketchybar")
-
-            -- ============================================================
-            -- LEFT
-            -- ============================================================
-
-            -- Yabai workspaces (spaces 1–10)
-            for i = 1, 10 do
-              sbar.add("space", "left", {
-                associated_space = tostring(i),
-                icon = { string = tostring(i), font = "SF Mono:Bold:14.0" },
-                label = { drawing = false },
-              })
-            end
-
-            -- Window title (from front_app script)
-            sbar.add("item", "window_title", "left", {
-              script = "sketchy-front_app",
-              update_freq = 1,
-              icon = { drawing = false },
-              label = { font = "SF Pro Display:Regular:12.0", padding_left = 5 },
-            })
-
-            -- ============================================================
-            -- CENTER
-            -- ============================================================
-
-            -- Date (left aligned in center block)
-            sbar.add("item", "date", "center", {
-              script = [[ date +"%m/%d/%Y" ]],
-              update_freq = 60,
-              icon = { drawing = false },
-              label = { font = "SF Pro Display:Bold:12.0", align = "left" },
-            })
-
-            -- Time (right aligned in center block)
-            sbar.add("item", "time", "center", {
-              script = [[ date +"%H:%M:%S" ]],
-              update_freq = 1,
-              icon = { drawing = false },
-              label = { font = "SF Pro Display:Bold:14.0", align = "right" },
-            })
-
-            -- ============================================================
-            -- RIGHT
-            -- ============================================================
-
-            -- Keyboard state
-            sbar.add("item", "keyboard", "right", {
-              script = "sketchy-keyboard",
-              update_freq = 2,
-              icon = { drawing = true },
-            })
-
-            -- Audio
-            sbar.add("item", "audio", "right", {
-              script = "sketchy-volume",
-              update_freq = 2,
-              icon = { drawing = true },
-            })
-
-            -- Network
-            sbar.add("item", "network", "right", {
-              script = "sketchy-network",
-              update_freq = 5,
-              icon = { drawing = true },
-            })
-
-            -- Backlight
-            sbar.add("item", "backlight", "right", {
-              script = "sketchy-backlight",
-              update_freq = 2,
-              icon = { drawing = true },
-            })
-
-            -- Battery
-            sbar.add("item", "battery", "right", {
-              script = "sketchy-battery",
-              update_freq = 30,
-              icon = { drawing = true },
-            })
-
-            -- ============================================================
-            -- BAR STYLE
-            -- ============================================================
-
-            sbar.config({
-              padding_left = 5,
-              padding_right = 5,
-              height = 28,
-              color = 0xff1e1e2e,   -- background color
-              corner_radius = 8,
-              blur_radius = 20,
-            })
-          '';
         };
       })
   ];
