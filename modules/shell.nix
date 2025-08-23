48998a0ba5270a82eb4e91844bb4af4c35076635
Chucklee1 <kermitthefrog@kakao.com>
2025-08-22 19:30:05 -0700

    added zsh-vim-mode plugin

--- modules/shell.nix
+++ modules/shell.nix
@@ -31,12 +31,23 @@
     })
   ];
   global.home = [
-    {
+    ({pkgs, ...}: {
       programs.zsh = {
         enable = true;
         syntaxHighlighting.enable = true;
+        plugins = [
+          {
+            name = "zsh-vim-mode";
+            src = pkgs.fetchFromGitHub {
+              owner = "softmoth";
+              repo = "zsh-vim-mode";
+              rev = "1f9953b7d6f2f0a8d2cb8e8977baa48278a31eab";
+              hash = "sha256-a+6EWMRY1c1HQpNtJf5InCzU7/RphZjimLdXIXbO6cQ=";
+            };
+          }
+        ];
       };
-    }
+    })
   ];
 
   linux.nix = [
