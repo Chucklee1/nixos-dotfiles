01938d503a65cd643aa668902c6d779f5a4cf2c4
Chucklee1 <kermitthefrog@kakao.com>
2025-08-24 17:54:10 -0700

    fixed latex errors on linux side
    todo: test on darwin

--- assets/emacs/latex.el
+++ assets/emacs/latex.el
@@ -1,27 +1,8 @@
-(use-package pdf-tools
-  :ensure t
-  :magic ("%PDF" . pdf-view-mode)
-  :config
-  (pdf-tools-install)) ;; compiles and sets up everything
-
 (use-package auctex
+  :ensure t
   :defer t
-  :after pdf-tools
-  :hook ((LaTeX-mode . TeX-source-correlate-mode) ;; enable synctex
-         (LaTeX-mode . TeX-PDF-mode))             ;; compile to PDF
-  :config
-  ;; (add-to-list 'exec-path "/Library/TeX/texbin")
-  ;; (setenv "PATH" (concat "/Library/TeX/texbin:" (getenv "PATH")))
-  )
+  :hook (LaTeX-mode . (lambda ()
+			(push (list 'output-pdf "Zathura")
+			      TeX-view-program-selection))))
 
-(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-        TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
-		TeX-source-correlate-start-server t)
-
-(add-hook 'TeX-after-compilation-finished-functions
-		  #'TeX-revert-document-buffer)
-
-(add-hook 'pdf-view-mode-hook (lambda ()
-                                (setq-local truncate-lines t)
-                                (visual-line-mode -1)
-                                (word-wrap nil)))
+(setq TeX-engine 'luatex)
