4d685a3c2a091a4bfc7996ee55a1f04fc4b4e996
Chucklee1 <kermitthefrog@kakao.com>
2025-09-05 21:12:51 -0700

    Updated desktop hardware
    modified:   modules/hardware.nix

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -5,40 +5,41 @@
 }: {
   desktop.nix = [
     {
-      fileSystems."/" = { 
-          device = "/dev/disk/by-label/WD_ROOT";
-          fsType = "btrfs";
-          options = [ "subvol=@nixos" ];
-        };
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/87f9df33-72a9-4841-9326-5200086e5f03";
+        fsType = "btrfs";
+        options = [ "subvol=@nixos" "compress=zstd" "relatime" ];
+      };
 
       fileSystems."/nix" = {
-          device = "/dev/disk/by-label/WD_ROOT";
-          fsType = "btrfs";
-          options = [ "subvol=@nix" ];
-        };
-
-      fileSystems."/srv" = {
-          device = "/dev/disk/by-label/BLUE_SATA";
-          fsType = "btrfs";
-          options = [ "subvol=@srv" ];
-        };
-
-      fileSystems."/opt" = {
-          device = "/dev/disk/by-label/BLUE_SATA";
-          fsType = "btrfs";
-          options = [ "subvol=@opt" ];
-        };
+        device = "/dev/disk/by-uuid/87f9df33-72a9-4841-9326-5200086e5f03";
+        fsType = "btrfs";
+        options = [ "subvol=@nix" "compress=zstd" "noatime" ];
+      };
 
       fileSystems."/boot" = {
-          device = "/dev/disk/by-label/WD_BOOT";
-          fsType = "vfat";
-          options = [ "fmask=0022" "dmask=0022" ];
-        };
+        device = "/dev/disk/by-uuid/AA80-5227";
+        fsType = "vfat";
+        options = [ "fmask=0022" "dmask=0022" ];
+      };
+
+      fileSystems."/opt" = {
+        device = "/dev/disk/by-uuid/ed8922cf-d9fc-41c5-aae4-3449e71626cc";
+        fsType = "btrfs";
+        options = [ "subvol=opt" "compress=zstd" "relatime" ];
+      };
+
+      fileSystems."/srv" = {
+        device = "/dev/disk/by-uuid/ed8922cf-d9fc-41c5-aae4-3449e71626cc";
+        fsType = "btrfs";
+        options = [ "subvol=srv" "compress=zstd" "relatime" ];
+      };
 
       swapDevices = [ ];
 
       boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
-      boot.kernelModules = ["kvm-amd"];
+      boot.kernelParams = ["amd_iommu=on" "iommu=pt"];
+      boot.kernelModules = ["kvm" "kvm_amd"];
       boot.supportedFilesystems = ["btrfs" "ext4" "ntfs" "zfs"];
       networking.hostId = "8425e349";
       hardware.cpu.amd.updateMicrocode = true;
