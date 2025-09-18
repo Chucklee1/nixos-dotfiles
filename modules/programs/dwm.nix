4f910cd6275c9c93b1fdffcb0b8c807b878ee0b7
Chucklee1 <kermitthefrog@kakao.com>
2025-09-18 14:47:08 -0700

    added picom & redshift for dwm

--- modules/programs/dwm.nix
+++ modules/programs/dwm.nix
@@ -13,12 +13,35 @@
         package = pkgs.dwm.overrideAttrs {src = inputs.dwm;};
         extraSessionCommands = ''
           ${pkgs.feh}/bin/feh --bg-scale ${config.stylix.image} &
-          ${pkgs.picom}/bin/picom --backend egl &
         '';
       };
     })
   ];
   dwm.home = [
-    ({pkgs, ...}: {home.packages = with pkgs; [dmenu];})
+    ({pkgs, ...}: {
+      home.packages = with pkgs; [dmenu];
+
+      services.picom = {
+        enable = true;
+        vSync = true;
+        backend = "glx";
+        opacityRules = [ "80:class_g = 'Emacs'" ];
+        settings = {
+          blur.method = "dual_kawase";
+        };
+      };
+
+      services.redshift = {
+        enable = true;
+        provider = "manual";
+        temperature.day = 3800;
+        temperature.night = 3800;
+        # Baruunbayan-Ulaan, Övörkhangai, Mongolia
+        settings.manual = {
+          lat=45.221;
+          lon=101.821;
+        };
+      };
+    })
   ];
 }
