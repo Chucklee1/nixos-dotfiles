ad84d69e86f2ff700c28a6a955a5ade7c72a68bf
Chucklee1 <kermitthefrog@kakao.com>
2025-09-19 17:00:48 -0700

    replaced rainbow nvim plugin with rainbow-delimiters

--- nixvim-config/plugins/ui.nix
+++ nixvim-config/plugins/ui.nix
@@ -3,7 +3,7 @@
     aerial.enable = true;
     kitty-navigator.enable = true;
     presence-nvim.enable = true;
-    rainbow.enable = true;
+    rainbow-delimiters.enable = true;
     scrollview.enable = true;
     trouble.enable = true;
     web-devicons.enable = true;
