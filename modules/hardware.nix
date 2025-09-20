59526e728827db896ac99673640fe1d95c2aa1f5
Chucklee1 <kermitthefrog@kakao.com>
2025-09-20 12:32:55 -0700

    Put WD disk UUID in a variable, added manual EFI entry for Arch
    modified:   modules/hardware.nix

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -4,63 +4,85 @@
   ...
 }: {
   desktop.nix = [
-    ({pkgs, ...}: {
+    (let
+      WD_UUID = "ff111615-41d4-4836-8adc-c9374e08bee9";
+      EVO_UUID = "5a5dcb04-31cb-4fae-8fbe-1e8e83a83501";
+    in {
       fileSystems."/" = {
-        device = "/dev/disk/by-uuid/ff111615-41d4-4836-8adc-c9374e08bee9";
+        device = "/dev/disk/by-uuid/${WD_UUID}";
         fsType = "btrfs";
-        options = ["subvol=WD/nixos" "compress=zstd" "relatime"];
+        options = ["subvol=WD/nixos" "relatime" "compress=zstd"];
       };
 
       fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/${WD_UUID}";
+        fsType = "btrfs";
+        options = ["subvol=WD/nixos/boot" "noatime"];
+      };
+
+      fileSystems."/boot/EFI" = {
         device = "/dev/disk/by-uuid/5F4E-A7BB";
         fsType = "vfat";
         options = ["fmask=0022" "dmask=0022"];
       };
 
       fileSystems."/nix" = {
-        device = "/dev/disk/by-uuid/ff111615-41d4-4836-8adc-c9374e08bee9";
+        device = "/dev/disk/by-uuid/${WD_UUID}";
         fsType = "btrfs";
         options = ["subvol=WD/nix" "noatime" "compress=zstd"];
       };
 
       fileSystems."/opt" = {
-        device = "/dev/disk/by-uuid/5a5dcb04-31cb-4fae-8fbe-1e8e83a83501";
+        device = "/dev/disk/by-uuid/${EVO_UUID}";
         fsType = "btrfs";
         options = ["subvol=EVO/opt" "relatime" "compress=zstd"];
       };
 
       fileSystems."/home/goat/SSE" = {
-        device = "/dev/disk/by-uuid/5a5dcb04-31cb-4fae-8fbe-1e8e83a83501";
+        device = "/dev/disk/by-uuid/${EVO_UUID}";
         fsType = "btrfs";
-        options = ["subvol=EVO/SSE"];
+        options = ["subvol=EVO/SSE" "relatime" "compress=zstd"];
       };
 
       fileSystems."/srv" = {
-        device = "/dev/disk/by-uuid/5a5dcb04-31cb-4fae-8fbe-1e8e83a83501";
+        device = "/dev/disk/by-uuid/${EVO_UUID}";
         fsType = "btrfs";
         options = ["subvol=EVO/srv" "relatime" "compress=zstd"];
       };
 
       fileSystems."/.snapshots/WD" = {
-        device = "/dev/disk/by-uuid/ff111615-41d4-4836-8adc-c9374e08bee9";
+        device = "/dev/disk/by-uuid/${WD_UUID}";
         fsType = "btrfs";
         options = ["subvol=WD/.snapshots" "relatime" "compress=zstd"];
       };
 
       fileSystems."/.snapshots/EVO" = {
-        device = "/dev/disk/by-uuid/5a5dcb04-31cb-4fae-8fbe-1e8e83a83501";
+        device = "/dev/disk/by-uuid/${EVO_UUID}";
         fsType = "btrfs";
         options = ["subvol=EVO/.snapshots" "relatime" "compress=zstd"];
       };
 
       swapDevices = [];
 
+      boot.loader.efi.efiSysMountPoint = "/boot/EFI";
+      boot.loader.grub.useOSProber = true;
+      # dual boot entry so I dont need a seperate bootloader for arch
+      boot.loader.grub.extraEntries = ''
+        menuentry "arch" {
+          insmod btrfs
+          search --no-floppy --fs-uuid --set=root ${WD_UUID}
+          linux /WD/arch/boot/vmlinuz-linux root=UUID=${WD_UUID} rw rootflags=subvol=WD/arch
+          initrd /WD/arch/boot/initramfs-linux.img
+        }
+      '';
+    })
+    {
       boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
       boot.kernelModules = ["kvm" "kvm_amd"];
       boot.supportedFilesystems = ["btrfs" "ext4" "ntfs"];
       hardware.cpu.amd.updateMicrocode = true;
       hardware.enableRedistributableFirmware = true;
-    })
+    }
   ];
 
   laptop.nix = [
