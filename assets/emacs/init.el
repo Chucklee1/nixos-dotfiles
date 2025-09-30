62d44389cfa4bcaba9b5a096ebaf8eba5499fd57
goat@darwin-macbook <kermitthefrog@kaokao.com>
2025-09-29 22:52:26 -0700

    Emacs: Moved from corfu to company
    Works better for me with evil and I like the minibuffer style
    Added complementary additions: cape, vertico, marginalia, nerd-icons
    for completion
    
    modified:   assets/emacs/init.el

--- assets/emacs/init.el
+++ assets/emacs/init.el
@@ -303,53 +303,58 @@
 	  (TeX-view))))
 
 (use-package apheleia
-  :ensure t
   :config
   (apheleia-global-mode t))
 
-(use-package corfu
-  :init
-  (global-corfu-mode))
-
-(setq read-extended-command-predicate #'command-completion-default-include-p)
-
-(use-package nerd-icons-corfu
-  :after corfu
-  :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
-
-(use-package corfu
-  ;; Optional customizations
+(use-package company
   :custom
-  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  (corfu-auto t)                 ;; Enable auto completion
-  (corfu-auto-prefix 2)          ;; Minimum length of prefix for auto completion.
-  (corfu-popupinfo-mode t)       ;; Enable popup information
-  (corfu-separator ?\s)          ;; Orderless field separator, Use M-SPC to enter separator
-  (completion-ignore-case t)
+  (company-idle-delay (lambda () (if (company-in-string-or-comment) nil 0.5)))
+  :hook
+  (after-init . global-company-mode))
 
-  ;; Emacs 30 and newer: Disable Ispell completion function.
-  ;; Try `cape-dict' as an alternative.
-  (text-mode-ispell-word-completion nil)
-
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (tab-always-indent 'complete)
-
-  (corfu-preview-current nil) ;; Don't insert completion without confirmation
-  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
-  ;; be used globally (M-/).  See also the customization variable
-  ;; `global-corfu-modes' to exclude certain modes.
+(use-package cape
   :init
-  (global-corfu-mode))
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
 
-(use-package nerd-icons-corfu
-  :after corfu
-  :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+  ;; The functions that are added later will be the first in the list
+  (add-hook 'completion-at-point-functions #'cape-dabbrev) ;; Complete word from current buffers
+  (add-hook 'completion-at-point-functions #'cape-dict) ;; Dictionary completion
+  (add-hook 'completion-at-point-functions #'cape-file) ;; Path completion
+  (add-hook 'completion-at-point-functions #'cape-elisp-block) ;; Complete elisp in Org or Markdown mode
+  (add-hook 'completion-at-point-functions #'cape-keyword) ;; Keyword completion
+
+  ;;(add-hook 'completion-at-point-functions #'cape-abbrev) ;; Complete abbreviation
+  ;;(add-hook 'completion-at-point-functions #'cape-history) ;; Complete from Eshell, Comint or minibuffer history
+  ;;(add-hook 'completion-at-point-functions #'cape-line) ;; Complete entire line from current buffer
+  ;;(add-hook 'completion-at-point-functions #'cape-elisp-symbol) ;; Complete Elisp symbol
+  ;;(add-hook 'completion-at-point-functions #'cape-tex) ;; Complete Unicode char from TeX command, e.g. \hbar
+  ;;(add-hook 'completion-at-point-functions #'cape-sgml) ;; Complete Unicode char from SGML entity, e.g., &alpha
+  ;;(add-hook 'completion-at-point-functions #'cape-rfc1345) ;; Complete Unicode char using RFC 1345 mnemonics
+  )
 
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package vertico
+  :init
+  (vertico-mode))
+
+(use-package marginalia
+  :after vertico
+  :init
+  (marginalia-mode))
+
+(use-package nerd-icons-completion
+  :after marginalia
+  :config
+  (nerd-icons-completion-mode)
+  :hook
+  ('marginalia-mode-hook . 'nerd-icons-completion-marginalia-setup))
 
 (use-package yasnippet
   :config
