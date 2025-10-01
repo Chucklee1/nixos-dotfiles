6bc423c30b75bc54f4b833611b20e5e1e74e9e76
goat@darwin-macbook <kermitthefrog@kaokao.com>
2025-09-30 18:59:13 -0700

    Added new function
    modified:   outputs/libs.nix

--- outputs/libs.nix
+++ outputs/libs.nix
@@ -3,7 +3,10 @@ with inputs.nixpkgs.lib; rec {
   readDirRecursive = dir:
     concatMapAttrs (file:
       flip getAttr {
-        directory = mapAttrs' (subpath: nameValuePair "${file}/${subpath}") (readDirRecursive "${dir}/${file}");
+        directory =
+          mapAttrs'
+            (subpath: nameValuePair "${file}/${subpath}")
+            (readDirRecursive "${dir}/${file}");
         regular = {
           ${file} = "${dir}/${file}";
         };
@@ -12,31 +15,26 @@ with inputs.nixpkgs.lib; rec {
 
   #absolute horrid deepMerge
   mergeAllRecursive = a: b:
-    foldl' (
-      acc: key: let
-        # sets all missing keys to null
-        va = a.${key} or null;
-        vb = b.${key} or null;
-      in
-        acc
-        // {
-          "${key}" =
-            # remove all nulls
-            if va == null
-            then vb
-            else if vb == null
-            then va
-            # concat all lists
-            else if isList va && isList vb
-            then va ++ vb
-            # recurse into function if attr
-            else if isAttrs va && isAttrs vb
-            then mergeAllRecursive va vb
-            # let last primitive-type values override previous
-            else vb;
-        }
-    ) {}
-    (unique (attrNames a ++ attrNames b));
+    foldl' (acc: key: let
+      # sets all missing keys to null
+      va = a.${key} or null;
+      vb = b.${key} or null;
+    in
+      acc // {
+        "${key}" =
+          if va == null
+          then vb
+          else if vb == null
+          then va
+          else if isList va && isList vb
+          then va ++ vb
+          # recurse into function if attr
+          else if isAttrs va && isAttrs vb
+          then mergeAllRecursive va vb
+          # let last primitive-type values override previous
+          else vb;
+      }) {}
+      (unique (attrNames a ++ attrNames b));
 
   # concats modules from imported files
   loadModules = dir: args: (pipe dir [
@@ -54,6 +52,25 @@ with inputs.nixpkgs.lib; rec {
     (filterAttrs (flip (const (hasSuffix ".nix"))))
     attrValues
   ]);
+
+  /*
+     - Given a root path, will return an attribute set
+       with the following rules:
+       - If the key ==
+         directory -> will call function again
+         file -> set value to full-file-path
+         directory AND is empty -> key is ignored in output
+         symlink -> key is ignored in output
+  */
+  pathsToAttrsets = root_dir: builtins.foldl' (acc: key:
+    lib.recursiveUpdate acc
+      (
+        lib.setAttrByPath
+          (lib.splitString "/" key)
+          (lib.getAttr key root_dir)
+      )
+  ) { } (builtins.readDir root_dir);
+
 
   # system helpers
   withSystem.ifDarwinElseLinux = system: A: B:
