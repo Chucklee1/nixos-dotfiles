14af08359ce81daa098976342b4e62738923b830
Chucklee1 <kermitthefrog@kakao.com>
2025-10-08 09:15:16 -0700

    Added zen browser module
    modified:   flake.nix
    new file:   modules/programs/zen-browser.nix

---
+++ modules/programs/zen-browser.nix
@@ -0,0 +1,28 @@
+{inputs, ...}: {
+  home = [
+    inputs.zen-browser.homeModules.twilight
+    {
+      programs.zen-browser = {
+        enable = true;
+        policies = {
+          AutofillAddressEnabled = false;
+          AutofillCreditCardEnabled = false;
+          DisableAppUpdate = true;
+          DisableFeedbackCommands = true;
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableTelemetry = true;
+          DontCheckDefaultBrowser = true;
+          NoDefaultBookmarks = true;
+          OfferToSaveLogins = false;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+        };
+      };
+    }
+  ];
+}
