ae37c834dc5164836878e5bfb4db21a417760d94
Chucklee1 <kermitthefrog@kakao.com>
2025-11-29 18:50:41 -0800

    Stylix: replaced manual base16 schemes with nix provided ones
    - Now using nord theme from pkgs.base16-schemes

--- modules/theming/stylix.nix
+++ modules/theming/stylix.nix
@@ -6,48 +6,6 @@
     )
     ({pkgs, ...}: let
       # helpers
-      hideHashtag = set: builtins.mapAttrs (_: v: builtins.replaceStrings ["#"] [""] v) set;
-      # themes
-      nordic = hideHashtag {
-        name = "nordic";
-        author = "AlexvZyl";
-        base00 = "#191D24";
-        base01 = "#242933";
-        base02 = "#3B4252";
-        base03 = "#4C566A";
-        base04 = "#D8DEE9";
-        base05 = "#E5E9F0";
-        base06 = "#ECEFF4";
-        base07 = "#8FBCBB";
-        base08 = "#BF616A";
-        base09 = "#D08770";
-        base0A = "#EBCB8B";
-        base0B = "#A3BE8C";
-        base0C = "#88C0D0";
-        base0D = "#81A1C1";
-        base0E = "#B48EAD";
-        base0F = "#C0C8D8";
-      };
-      clasic-dark = hideHashtag {
-        name = "Classic Dark";
-        author = "Jason Heeris";
-        base00 = "#151515";
-        base01 = "#202020";
-        base02 = "#303030";
-        base03 = "#505050";
-        base04 = "#B0B0B0";
-        base05 = "#D0D0D0";
-        base06 = "#E0E0E0";
-        base07 = "#F5F5F5";
-        base08 = "#AC4142";
-        base09 = "#D28445";
-        base0A = "#F4BF75";
-        base0B = "#90A959";
-        base0C = "#75B5AA";
-        base0D = "#6A9FB5";
-        base0E = "#AA759F";
-        base0F = "#8F5536";
-      };
       painted-lake = pkgs.fetchurl {
         url = "https://w.wallhaven.cc/full/5g/wallhaven-5g22q5.png";
         hash = "sha256-snqkeQecU0opsBfIrnkl6aiV71hSCmqnZBAsibNG4w8=";
@@ -66,7 +24,7 @@
         autoEnable = true;
         homeManagerIntegration.autoImport = true;
         image = painted-lake;
-        base16Scheme = nordic;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
         polarity = "dark";
         opacity.desktop = 0.8;
         opacity.terminal = 0.8;
