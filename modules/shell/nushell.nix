6b8c6bf13176927ae311fe4d20f24e12237bd403
Chucklee1 <kermitthefrog@kakao.com>
2025-11-29 23:26:32 -0800

    manually added shell asiases for nushell

--- modules/shell/nushell.nix
+++ modules/shell/nushell.nix
@@ -3,10 +3,19 @@
     ({lib, pkgs, user, ...}: {users.users.${user}.shell = lib.mkForce pkgs.nushell;})
   ];
   home = [
-    ({pkgs, ...}:{
-	    programs.nushell = {
+    ({config, pkgs, machine, ...}: {
+      programs.nushell = {
         enable = true;
-        settings = {
+        # need to manually set shellAliases with nushell...
+        shellAliases = let
+          rebuild_cmd =
+            if machine == "macbook"
+            then "darwin-rebuild"
+            else "nixos-rebuild";
+        in {
+          rebuild-flake = "sudo ${rebuild_cmd} switch --flake ${config.home.homeDirectory}/nixos-dotfiles#${machine} --show-trace --impure";
+        };
+	      settings = {
           show_banner = false; # remove startup msg prompt
           edit_mode = "vi";
         };
@@ -19,9 +28,10 @@
       # once I figure out nushell coding I will make my own prompt...
       programs.oh-my-posh = {
         enable = true;
+        enableBashIntegration = false;
+        enableZshIntegration = false;
         useTheme = "pure";
 	    };
     })
   ];
-
 }
