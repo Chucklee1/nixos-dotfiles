ea6de3e7d00363f728172cd1cc303a872afe4e72
Chucklee1 <kermitthefrog@kakao.com>
2025-12-07 18:47:40 -0800

    Java: moved gradle back from home to nix scope, added jdt lsp

--- modules/software/java.nix
+++ modules/software/java.nix
@@ -6,13 +6,14 @@
         jdk
         javaPackages.compiler.temurin-bin.jdk-25
         # build system that fabric uses
+        gradle
         gradle-completion
         # better java
         kotlin
         kotlin-language-server
+        jdt-language-server
         tree-sitter-grammars.tree-sitter-kotlin
       ];
 	  })
   ];
-  home = [{programs.gradle.enable = true;}];
 }
