b34d8bfad494385b81630352dc8272d7907c7285
Chucklee1 <kermitthefrog@kakao.com>
2025-12-11 19:19:26 -0800

    removed file:    assets/arch folder

--- assets/arch/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin sh
-
-ASSET_DIR=$HOME/nixos-dotfiles/assets/arch
-# in case xdg isn't configured yet
-CFG_DIR=$HOME/.config
-
-for i in "$ASSET_DIR"; do
-	ln -s "$ASSET_DIR/${i##*/}" "$CFG_DIR/${i##*/}"
