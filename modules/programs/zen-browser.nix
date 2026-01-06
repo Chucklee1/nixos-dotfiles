2ecc0eb6c86e9f7edc6533e652c86d554da939c6
Chucklee1 <kermitthefrog@kakao.com>
2026-01-06 14:39:55 -0800

    modified:   modules/programs/zen-browser.nix

--- modules/programs/zen-browser.nix
+++ modules/programs/zen-browser.nix
@@ -1,13 +1,13 @@
 {inputs, ...}: {
-    nix = [
+  nix = [
     {nixpkgs.overlays = [inputs.nur.overlays.default];}
-    # {environment.variables.BROWSER = "";}
   ];
 
   home = [
     inputs.zen-browser.homeModules.twilight
     ({pkgs, ...}: {
-      stylix.targets.librewolf.profileNames = ["default"];
+      stylix.targets.zen-browser.profileNames = ["default"];
+      home.sessionVariables.BROWSER = "zen";
       programs.zen-browser = {
         enable = true;
         policies = {
@@ -29,42 +29,42 @@
           };
           profiles.default = {
             name = "default";
-          isDefault = true;
-          search = {
-            force = true;
-            default = "ddg";
-            engines = {
-              "Nix Packages" = {
-                urls = [
-                  {
-                    template = "https://search.nixos.org/packages";
-                    params = [
-                      {name = "type"; value = "packages";}
-                      {name = "query"; value = "{searchTerms}";}
-                    ];
-                  }
-                ];
-                icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = ["@np"];
-              };
-              "NixOS Wiki" = {
-                urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-                icon = "https://nixos.wiki/favicon.png";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = ["@nw"];
-              };
-              "MyNixOS" = {
-                urls = [{template = "https://mynixos.com/search?q={searchTerms}";}];
-                icon = "https://mynixos.com/static/icons/mnos-logo.svg";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = ["@mn"];
+            isDefault = true;
+            search = {
+              force = true;
+              default = "ddg";
+              engines = {
+                "Nix Packages" = {
+                  urls = [
+                    {
+                      template = "https://search.nixos.org/packages";
+                      params = [
+                        {name = "type"; value = "packages";}
+                        {name = "query"; value = "{searchTerms}";}
+                      ];
+                    }
+                  ];
+                  icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                  definedAliases = ["@np"];
+                };
+                "NixOS Wiki" = {
+                  urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+                  icon = "https://nixos.wiki/favicon.png";
+                  updateInterval = 24 * 60 * 60 * 1000; # every day
+                  definedAliases = ["@nw"];
+                };
+                "MyNixOS" = {
+                  urls = [{template = "https://mynixos.com/search?q={searchTerms}";}];
+                  icon = "https://mynixos.com/static/icons/mnos-logo.svg";
+                  updateInterval = 24 * 60 * 60 * 1000; # every day
+                  definedAliases = ["@mn"];
+                };
               };
             };
+            extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [ublock-origin];
           };
-          extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [ublock-origin];
         };
       };
-};
     })
     # from repo's readme
     ({pkgs, ...}: {
