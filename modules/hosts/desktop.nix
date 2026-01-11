531dbcb1f7d4fc77cc804c935b835b41fb937796
Chucklee1 <kermitthefrog@kakao.com>
2026-01-11 13:04:42 -0800

    modified:   modules/hosts/desktop.nix

--- modules/hosts/desktop.nix
+++ modules/hosts/desktop.nix
@@ -63,25 +63,24 @@ in {
     # gpu
     ({lib, config, ...}: with lib;
       /**** global nvidia toggle switch ****/
-      let enableNvidiaCard = false; in
+      let enableNvidiaCard = true; in
       /**** nvidia + radeon ****/
       (if enableNvidiaCard then {
         services.xserver.videoDrivers = ["nvidia" "amdgpu"];
         nixpkgs.config.nvidia.acceptLicense = true;
         hardware.nvidia = {
-          package = config.boot.kernelPackages.nvidiaPackages.beta;
           modesetting.enable = true;
           powerManagement.enable = false;
           powerManagement.finegrained = false;
           videoAcceleration = true;
           open = false;
         };
-        environment.variables = {
-          LIBVA_DRIVER_NAME = "nvidia";
-          NVD_BACKEND = "direct";
-          GBM_BACKEND = "nvidia-drm";
-          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-        };
+        # environment.variables = {
+          # LIBVA_DRIVER_NAME = "nvidia";
+          # NVD_BACKEND = "direct";
+          # GBM_BACKEND = "nvidia-drm";
+          # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+        # };
       }
         /**** blacklist nvidia for vm usage ****/
        else {
