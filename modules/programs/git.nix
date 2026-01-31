c73b81291212df4a6ea784d292f4c5675f9ef120
Chucklee1 <kermitthefrog@kakao.com>
2026-01-30 23:39:08 -0800

    modified:   modules/programs/git.nix

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -3,8 +3,21 @@
     {
       programs.git = {
         enable = true;
-        settings.user.email = "kermitthefrog@kakao.com";
-        settings.user.name = "Chucklee1";
+        settings = {
+          user.email = "kermitthefrog@kakao.com";
+          user.name = "Chucklee1";
+          alias = {
+            aa = "add -A";
+            ce = "commit -e";
+            fix = "commit --fixup HEAD";
+            reword = "commit --amend";
+            squash = "rebase --autosquash HEAD~2";
+            unstage = "restore --staged";
+            undo = "reset --soft HEAD~1";
+            st = "status -sb";
+            lg = "log --oneline --graph --decorate --all";
+          };
+        };
       };
       programs.lazygit = {
         enable = true;
