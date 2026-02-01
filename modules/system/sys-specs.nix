9ab9c6d7b50e15958ec48a02a510829e698a1c19
Chucklee1 <kermitthefrog@kakao.com>
2026-01-31 22:05:45 -0800

    modified:   modules/system/sys-specs.nix

--- modules/system/sys-specs.nix
+++ modules/system/sys-specs.nix
@@ -9,7 +9,7 @@
       };
       networking.firewall = {
         enable = true;
-        allowedTCPPorts = [22 80 443 8384];
+        allowedTCPPorts = [22 80 443 8384 8000];
       };
       i18n.defaultLocale = "en_CA.UTF-8";
       time.timeZone = "America/Vancouver";
