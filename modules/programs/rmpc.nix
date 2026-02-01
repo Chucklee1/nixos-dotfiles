28c957f099ca3b9d933aa047bfe7f0f0547a2d52
Chucklee1 <kermitthefrog@kakao.com>
2026-01-31 22:19:55 -0800

    re-added rmpc
    hopefully will get it to work with nfs...
    
    new file:   assets/rmpc/config.ron
    new file:   assets/rmpc/themes/theme.ron
    new file:   modules/programs/rmpc.nix

---
+++ modules/programs/rmpc.nix
@@ -0,0 +1,21 @@
+{self, ...}: {
+  home = [({pkgs, ...}: {
+    services.mpd = let
+      dir = "/srv/media";
+    in {
+      enable = true;
+      musicDirectory = "${dir}/Music";
+      playlistDirectory = "${dir}/Music/playlist";
+      network.listenAddress = "any";
+      extraConfig = ''
+        save_absolute_paths_in_playlists "yes"
+          audio_output {
+            type "pipewire"
+            name "MPDOUT"
+          }
+      '';
+    };
+    home.packages = [pkgs.rmpc];
+    home.file.".config/rmpc".source = "${self}/assets/rmpc";
+  })];
+}
