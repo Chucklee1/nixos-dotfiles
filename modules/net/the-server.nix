e82b6b92bc5c7f69265056ec51fa56ff2ba059ff
Chucklee1 <kermitthefrog@kakao.com>
2026-01-31 22:05:40 -0800

    modified:   modules/net/the-server.nix

--- modules/net/the-server.nix
+++ modules/net/the-server.nix
@@ -1,12 +1,20 @@
 {
   nix = [
-    {
-      services.navidrome = {
-        enable = true;
-        settings.Address = "localhost";
-        settings.MusicFolder = "/srv/media/Music";
-      };
-      services.audiobookshelf.enable = true; # port = 8000;
-    }
+    (let
+      music_dir = "/srv/media/Music";
+    in
+      {
+        fileSystems."/srv/shared" = {
+          device = "nixos-inspiron:${music_dir}";
+          fsType = "nfs";
+          options = [ "defaults" "auto" "_netdev" ];
+        };
+        services.navidrome = {
+          enable = true;
+          settings.Address = "localhost";
+          settings.MusicFolder = music_dir;
+        };
+        services.audiobookshelf.enable = true; # port = 8000;
+      })
   ];
 }
