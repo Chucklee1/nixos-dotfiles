d02cdbe043ffdfd0067a59e3f7fc44acee633086
Chucklee1 <kermitthefrog@kakao.com>
2026-02-17 20:44:26 -0800

    modified:   modules/system/pkgconfig.nix

--- modules/system/pkgconfig.nix
+++ modules/system/pkgconfig.nix
@@ -1,7 +1,15 @@
 {
   nix = [
     ({lib, ...}: {
-      nix.settings.experimental-features = "nix-command flakes";
+      nix.settings = {
+        experimental-features = "nix-command flakes";
+        substituters = [
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+      };
       nix.gc = {
         automatic = lib.mkDefault true;
         options = lib.mkDefault "--delete-older-than 7d";
