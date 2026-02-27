49c53e2208c44149ca5ed38cd0a5c9f101a4b824
Chucklee1 <kermitthefrog@kakao.com>
2026-02-26 19:58:45 -0800

    new file:   modules/drivers/fcitx.nix

---
+++ modules/drivers/fcitx.nix
@@ -0,0 +1,13 @@
+{
+  nix = [
+    ({pkgs, ...}: {
+      i18n.inputMethod = {
+        enabled = "fcitx5";
+        fcitx5.addons = with pkgs; [
+          fcitx5-hangul
+          fcitx5-nord
+        ];
+      };
+    })
+  ];
+}
