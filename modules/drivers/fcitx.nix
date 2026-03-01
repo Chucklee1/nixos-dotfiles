db4709053cf6f480a72ef09cd63847193d019404
Chucklee1 <kermitthefrog@kakao.com>
2026-02-28 17:25:34 -0800

    modified:   modules/drivers/fcitx.nix

--- modules/drivers/fcitx.nix
+++ modules/drivers/fcitx.nix
@@ -2,7 +2,8 @@
   nix = [
     ({pkgs, ...}: {
       i18n.inputMethod = {
-        enabled = "fcitx5";
+        enable = true;
+        type = "fcitx5";
         fcitx5.addons = with pkgs; [
           fcitx5-hangul
           fcitx5-nord
