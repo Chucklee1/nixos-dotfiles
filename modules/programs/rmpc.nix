df639b5f5f84805575e661c65d2332c99eaf48e8
Chucklee1 <kermitthefrog@kakao.com>
2026-03-01 19:44:09 -0800

    modified:   modules/programs/rmpc.nix

--- modules/programs/rmpc.nix
+++ modules/programs/rmpc.nix
@@ -18,7 +18,13 @@
       services.mpdris2 = {
         enable = true;
         multimediaKeys = true;
+        # idk why but it works when I manually define here
+        mpd.port = 6600;
+        mpd.host = "127.0.0.1";
       };
+
+      # scrobbing
+      services.listenbrainz-mpd.enable = true;
 
       # rmpc
       home.packages = [pkgs.rmpc];
