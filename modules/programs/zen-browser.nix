737f54d42b87e4ee2b924babdaf414f8be685e1b
Chucklee1 <kermitthefrog@kakao.com>
2026-03-10 22:05:28 -0700

    modified:   modules/programs/zen-browser.nix

--- modules/programs/zen-browser.nix
+++ modules/programs/zen-browser.nix
@@ -30,6 +30,12 @@
           profiles.default = {
             name = "default";
             isDefault = true;
+            settings = {
+              "zen.view.compact.hide-tabbar" = true;
+              "zen.view.compact.hide-toolbar" = true;
+              "zen.welcome-screen.seen" = true;
+              "zen.urlbar.behavior" = "normal";
+            };
             search = {
               force = true;
               default = "ddg";
@@ -70,38 +76,6 @@
             extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [ublock-origin];
           };
         };
-      };
-    })
-    # from repo's readme
-    ({pkgs, ...}: {
-      xdg.mimeApps = let
-        value = let
-          zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.system}.twilight; # or twilight
-        in
-          zen-browser.meta.desktopFileName;
-
-        associations = builtins.listToAttrs (map (name: {
-            inherit name value;
-          }) [
-            "application/x-extension-shtml"
-            "application/x-extension-xhtml"
-            "application/x-extension-html"
-            "application/x-extension-xht"
-            "application/x-extension-htm"
-            "x-scheme-handler/unknown"
-            "x-scheme-handler/mailto"
-            "x-scheme-handler/chrome"
-            "x-scheme-handler/about"
-            "x-scheme-handler/https"
-            "x-scheme-handler/http"
-            "application/xhtml+xml"
-            "application/json"
-            "text/plain"
-            "text/html"
-          ]);
-      in {
-        associations.added = associations;
-        defaultApplications = associations;
       };
     })
   ];
