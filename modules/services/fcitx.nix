03f8288606d5f4800bde9a9759935b81fe16919d
Chucklee1 <kermitthefrog@kakao.com>
2026-03-12 00:21:16 -0700

    modified:   modules/services/fcitx.nix

--- modules/services/fcitx.nix
+++ modules/services/fcitx.nix
@@ -1,13 +1,15 @@
 {
   nix = [
-    ({pkgs, ...}: {
+    ({config, pkgs, ...}: {
       i18n.inputMethod = {
         enable = true;
         type = "fcitx5";
-        fcitx5.addons = with pkgs; [
-          fcitx5-hangul
-          fcitx5-nord
-        ];
+        fcitx5 = {
+          addons = [pkgs.fcitx5-hangul];
+          waylandFrontend = config.programs.niri.enable;
+          # conflicts with emacs
+          settings.addons.clipboard.globalSection.PastePrimaryKey = "";
+        };
       };
     })
   ];
