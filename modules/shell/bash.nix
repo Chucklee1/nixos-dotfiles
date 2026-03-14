de97062fb55c197687ffb26770bd1fb77c2479cf
Chucklee1 <kermitthefrog@kakao.com>
2026-03-14 11:44:03 -0700

    new file:   modules/shell/bash.nix

---
+++ modules/shell/bash.nix
@@ -0,0 +1,12 @@
+{
+  nix = [
+    {
+      programs.bash = {
+        enable = true;
+      };
+        
+      # Average "simple" posix complient shell prompt
+      enviornment.variable.PS1 = "\033[31m┌─[\033[0m\033[1m\u\033[31m]\033[35m \w\n\033[31m└>\033[0m ";
+    }
+  ];
+}
