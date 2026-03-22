1ff425675f972138e968919dfe5a5557d37c115a
Chucklee1 <kermitthefrog@kakao.com>
2026-03-22 11:51:21 -0700

    merged xserver with dwm
    modified:   modules/programs/dwm.nix
    deleted:    modules/services/xserver.nix

--- modules/programs/dwm.nix
+++ modules/programs/dwm.nix
@@ -1,10 +1,51 @@
 {inputs, ...}: {
   nix = [
     ({pkgs, ...}: {
+      services.xserver.enable = true;
       services.xserver.windowManager.dwm = {
         enable = true;
         package = pkgs.dwm.overrideAttrs {src = inputs.dwm;};
       };
     })
   ];
+
+  home = [
+    ({config, pkgs, ...}: {
+      home.packages = with pkgs; [
+        dmenu
+        xev
+      ];
+
+      xsession.enable = true;
+      xsession.initExtra = ''
+        ${pkgs.feh}/bin/feh --bg-scale ${config.stylix.image} &
+      '';
+
+      services.picom = {
+        enable = true;
+        vSync = true;
+        backend = "glx";
+        opacityRules = [
+          "80:class_g = 'Emacs'"
+          "80:class_g = 'Kitty'"
+        ];
+        settings = {
+          blur.method = "dual_kawase";
+        };
+      };
+
+      services.redshift = {
+        enable = true;
+        provider = "manual";
+        temperature.day = 3800;
+        temperature.night = 3800;
+        # Baruunbayan-Ulaan, Övörkhangai, Mongolia
+        settings.manual = {
+          lat = 45.221;
+          lon = 101.821;
+        };
+      };
+    })
+  ];
+
 }
