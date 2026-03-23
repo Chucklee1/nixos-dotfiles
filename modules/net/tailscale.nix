882e0d4a6be9905685bb9ab66a91da313e8b34ef
Chucklee1 <kermitthefrog@kakao.com>
2026-03-23 09:55:30 -0700

    tailscale:   removed systray modified:   modules/net/tailscale.nix

--- modules/net/tailscale.nix
+++ modules/net/tailscale.nix
@@ -1,7 +1,1 @@
-{
-  nix = [{services.tailscale.enable = true;}];
-  home = [({pkgs, ...}: {
-      services.tailscale-systray.enable = pkgs.stdenv.isLinux;
-    })  
-  ];
-}
+{ nix = [{services.tailscale.enable = true;}]; }
