9fd4b8c6b1cf96f92e9cc61c5a0942cb3959f524
Chucklee1 <kermitthefrog@kakao.com>
2026-03-24 19:16:29 -0700

    modified:   modules/shell/bash.nix

--- modules/shell/bash.nix
+++ modules/shell/bash.nix
@@ -1,12 +1,17 @@
 {
   nix = [
     {
-      programs.bash = {
+      programs.bash = let
+      set_attr = col: str: "\\033[${toString col}m${str}\\033[0m";
+      red = str: set_attr 31 str;
+      purple = str: set_attr 35 str;
+      bf = str: set_attr 1 str;
+        in {
         enable = true;
+        promptInit = ''
+          PS1='${red "┌─["}${bf "\\u"}${red "]"} ${purple "\\w"}\n${red "└>"} '
+        '';
       };
-        
-      # Average "simple" posix complient shell prompt
-      environment.variable.PS1 = "\033[31m┌─[\033[0m\033[1m\u\033[31m]\033[35m \w\n\033[31m└>\033[0m ";
     }
   ];
 }
