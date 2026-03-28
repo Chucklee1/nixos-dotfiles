256ab29e3d9c06c0800036f48221feadc25ec68e
Chucklee1 <kermitthefrog@kakao.com>
2026-03-28 08:20:32 -0700

    modified:   modules/system/sops.nix

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -4,7 +4,7 @@
     ({config, pkgs, user, ...}: {
       environment.systemPackages = [pkgs.sops];
       environment.variables.SOPS_AGE_KEY_FILE = config.sops.age.keyFile;
-      sops.age.generateKeyFile = false;
+      sops.age.generateKey = false;
       # note: must rebuild system for secrets.yaml changes to take affect
       sops.defaultSopsFile = ../../secrets.yaml;
       sops.defaultSopsFormat = "yaml";
