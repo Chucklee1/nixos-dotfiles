8ac6548f9304695b0be35a621d9e0005b98ed568
Chucklee1 <kermitthefrog@kakao.com>
2026-03-28 08:12:23 -0700

    modified:   flake/devshells.nix

--- flake/devshells.nix
+++ flake/devshells.nix
@@ -4,7 +4,7 @@
   ...
 }:
 with pkgs; let
-  std = msg: ''echo -e "\e[32m${msg}\e[0m" '';
+  msg = str: ''echo -e "\e[32m${str}\e[0m" '';
 in {
   haskell = mkShell {
     packages = [ghc cabal-install stack zlib];
@@ -18,14 +18,53 @@ in {
     packages = [just];
     shellHook = ''
       export make="just"
-      ${std "Loaded dotifles env"}
+      ${msg "Loaded dotifles env"}
+    '';
+  };
+  qt = mkShell {
+      nativeBuildInputs = [
+        (qt6.env "qt6-simc-${qt6.qtbase.version}" [
+          qt6.qtbase
+          qt6.qmake
+          qt6.qt5compat
+          qt6.qttools
+          qt6.qtdeclarative
+          qt6.qtcharts
+          qt6.qtwayland
+          qt6.qtmultimedia
+          qt6.qtlanguageserver
+          qt6.qtquick3d
+          qt6.qtshadertools
+          qt6.qtquicktimeline
+        ])
+        clang
+        cmake
+        gnumake
+        gdb
+        libglvnd
+        ninja
+        pkg-config
+      ];
+
+    shellHook = ''
+      # tmp fix for fish, ignore for now
+      export IN_NIX_SHELL=1
+
+      export CC=${pkgs.clang}/bin/clang
+      export CXX=${pkgs.clang}/bin/clang++
+
+      export CMAKE_C_COMPILER=$CC
+      export CMAKE_CXX_COMPILER=$CXX
+      export CMAKE_GENERATOR=Ninja
+
+      ${msg "Loaded qt env"}
     '';
   };
   remote = mkShell {
     packages = [just inputs.nix-vim.packages.${pkgs.system}.core];
     shellHook = ''
       export make="just"
-      ${std "Loaded remote env"}
+      ${msg "Loaded remote env"}
     '';
   };
   crossComp = let
@@ -39,8 +78,7 @@ in {
         cpkgs.binutils
       ];
       shellHook = ''
-
-        ${std "Loaded crossComp env for arch: ${CROSS_ARCH}"}
+        ${msg "Loaded crossComp env for arch: ${CROSS_ARCH}"}
       '';
     };
 }
