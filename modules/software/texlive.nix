db208ff42ccd9981bb766dacc252445be4693f15
Chucklee1 <kermitthefrog@kakao.com>
2026-03-30 18:38:03 -0700

    modified:   modules/software/texlive.nix

--- modules/software/texlive.nix
+++ modules/software/texlive.nix
@@ -4,55 +4,7 @@
       environment.systemPackages = [
         # can be used in org/latex docs
         pkgs.gnuplot
-        (pkgs.texlive.combine {
-          inherit
-            (pkgs.texlive)
-            # core
-            scheme-small
-            latex
-            latexmk
-            preview
-            # font
-            capt-of
-            fontawesome5
-            fontspec
-            unicode-math
-            microtype
-            titlesec
-            setspace
-            parskip
-            paracol
-            # bibliography
-            biblatex
-            biber
-            csquotes
-            # image stuff
-            dvisvgm
-            dvipng
-            # math
-            amsmath
-            amsfonts
-            mathtools
-            # plotting
-            pgfplots
-            asymptote
-            # graphics
-            graphics
-            xcolor
-            geometry
-            wrapfig
-            float
-            caption
-            # text candy
-            ulem
-            hyperref
-            # lua
-            luatex
-            luacode
-            lualatex-math
-            luaotfload
-            ;
-        })
+        pkgs.texlive.combined.scheme-full
       ];
     })
   ];
