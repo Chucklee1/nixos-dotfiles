626e9724d839baeb52537b9e2dd018ebe915bfe2
Chucklee1 <kermitthefrog@kakao.com>
2026-04-18 11:08:57 -0700

    modified:   modules/services/sunshine.nix

--- modules/services/sunshine.nix
+++ modules/services/sunshine.nix
@@ -1,11 +1,22 @@
 {
   nix = [
-    {
+    ({lib, ...}: {
       services.sunshine = {
         enable = true;
         capSysAdmin = true;
         openFirewall = true;
       };
-    }
+
+      networking.firewall = {
+        allowedTCPPorts = lib.mkAfter [ 47984 47989 47990 48010 ];
+        allowedUDPPortRanges = lib.mkAfter [
+          { from = 47998; to = 48000; }
+          { from = 8000; to = 8010; }
+        ];
+      };
+      # fix avahi stuff
+      services.avahi.publish.enable = true;
+      services.avahi.publish.userServices = true;
+    })
   ];
 }
