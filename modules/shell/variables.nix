48d42197a7de318386d09d00b0385c4b91c10c4e
Chucklee1 <kermitthefrog@kakao.com>
2026-06-14 09:34:13 -0700

    modified:   modules/shell/variables.nix

--- modules/shell/variables.nix
+++ modules/shell/variables.nix
@@ -9,6 +9,7 @@
             else "nixos-rebuild";
         in {
           rebuild-flake = "sudo ${rebuild_cmd} switch --flake $HOME/nixos-dotfiles#${machine} --show-trace --impure";
+          flopts = "nixos-option --flake $HOME/nixos-dotfiles#${machine} --show-trace --impure";
           sysctl = "sudo systemctl";
           usrctl = "systemctl --user";
         };
