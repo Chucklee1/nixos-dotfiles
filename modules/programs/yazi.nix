a33f34cb62a90b70b125e2b4e2cc03423ebf4ecf
Chucklee1 <kermitthefrog@kakao.com>
2026-07-14 22:53:25 -0700

    modified:   modules/programs/yazi.nix

--- modules/programs/yazi.nix
+++ modules/programs/yazi.nix
@@ -1,10 +1,6 @@
 {
   home = [
-    ({
-      config,
-      pkgs,
-      ...
-    }: {
+    ({pkgs, ...}: {
       programs.yazi = {
         enable = true;
         shellWrapperName = "y";
@@ -35,82 +31,6 @@
             max_width = 1000;
             max_height = 1000;
           };
-          plugin.append_fetchers = [
-            {
-              id = "git";
-              name = "*";
-              run = "git";
-            }
-            {
-              id = "git";
-              name = "*/";
-              run = "git";
-            }
-          ];
-          plugin.append_preloaders = [
-            {
-              mime = "{audio,video,image}/*";
-              run = "mediainfo";
-            }
-            {
-              mime = "application/subrip";
-              run = "mediainfo";
-            }
-          ];
-          plugin.append_previewers = [
-            {
-              mime = "{audio,video,image}/*";
-              run = "mediainfo";
-            }
-            {
-              mime = "application/subrip";
-              run = "mediainfo";
-            }
-            {
-              mime = "application/*zip";
-              run = "ouch";
-            }
-            {
-              mime = "application/x-tar";
-              run = "ouch";
-            }
-            {
-              mime = "application/x-bzip2";
-              run = "ouch";
-            }
-            {
-              mime = "application/x-7z-compressed";
-              run = "ouch";
-            }
-            {
-              mime = "application/x-rar";
-              run = "ouch";
-            }
-            {
-              mime = "application/vnd.rar";
-              run = "ouch";
-            }
-            {
-              mime = "application/x-xz";
-              run = "ouch";
-            }
-            {
-              mime = "application/xz";
-              run = "ouch";
-            }
-            {
-              mime = "application/x-zstd";
-              run = "ouch";
-            }
-            {
-              mime = "application/zstd";
-              run = "ouch";
-            }
-            {
-              mime = "application/java-archive";
-              run = "ouch";
-            }
-          ];
         };
 
         keymap = {
@@ -156,53 +76,6 @@
               run = "shell --block -- clear && trash-restore --overwrite";
             }
           ];
-        };
-
-        # stolen from: kerfuzzle's fork of stylix on github
-        # file:        yazi-v26.1.22/modules/yazi/hm.nix
-        # commit:      07f41f942ae752ab3b9f9071e45d30b37e4c0785
-        theme = with config.lib.stylix.colors.withHashtag; {
-          icon = let
-            mkIcon = text: fg: {inherit text fg;};
-          in {
-            dirs = let
-              mkDirIcon = name: text: fg: ((mkIcon text fg) // {inherit name;});
-            in [
-              (mkDirIcon ".config" "" orange)
-              (mkDirIcon ".git" "" cyan)
-              (mkDirIcon ".github" "" blue)
-              (mkDirIcon ".npm" "" blue)
-              (mkDirIcon "Desktop" "" cyan)
-              (mkDirIcon "Development" "" cyan)
-              (mkDirIcon "Documents" "" cyan)
-              (mkDirIcon "Downloads" "" cyan)
-              (mkDirIcon "Library" "" cyan)
-              (mkDirIcon "Movies" "" cyan)
-              (mkDirIcon "Music" "" cyan)
-              (mkDirIcon "Pictures" "" cyan)
-              (mkDirIcon "Public" "" cyan)
-              (mkDirIcon "Videos" "" cyan)
-            ];
-
-            conds = let
-              mkCondsIcon = cond: text: fg: ((mkIcon text fg) // {"if" = cond;});
-            in [
-              # Special files
-              (mkCondsIcon "orphan" "" base05)
-              (mkCondsIcon "link" "" base04)
-              (mkCondsIcon "block" "" yellow)
-              (mkCondsIcon "char" "" yellow)
-              (mkCondsIcon "fifo" "" yellow)
-              (mkCondsIcon "sock" "" yellow)
-              (mkCondsIcon "sticky" "" yellow)
-              (mkCondsIcon "dummy" "" red)
-
-              # Fallback
-              (mkCondsIcon "dir" "" blue)
-              (mkCondsIcon "exec" "" green)
-              (mkCondsIcon "!dir" "" base05)
-            ];
-          };
         };
       };
     })
