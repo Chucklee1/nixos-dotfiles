1878bd4b9dae960129f0016465f99b51d42db902
Chucklee1 <kermitthefrog@kakao.com>
2026-08-06 14:51:44 -0700

    updating cachix for prismLauncher & cachyos-kernel
    modified:   modules/games/prismLauncher.nix
    modified:   modules/hardware/cachyos-kernel.nix

--- modules/hardware/cachyos-kernel.nix
+++ modules/hardware/cachyos-kernel.nix
@@ -1,8 +1,8 @@
 {inputs, ...}: {
   nix = [
     ({pkgs, ...}: {
-      nix.settings.substituters = ["https://cache.garnix.io"];
-      nix.settings.trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+      nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
+      nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
       nixpkgs.overlays = [inputs.nix-cachyos-kernel.overlays.pinned];
       boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
     })
