67148477ba6f4d9b91e36ee48544f03da92c550e
Chucklee1 <kermitthefrog@kakao.com>
2026-09-15 18:22:00 -0700

    Updated desktop cuda cachix

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -296,10 +296,10 @@ with mod; {
       nixpkgs.config.cudaSupport = true;
       nix.settings = {
         substituters = [
-          "https://cuda-maintainers.cachix.org"
+          "https://cache.nixos-cuda.org"
         ];
         trusted-public-keys = [
-          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         ];
       };
     }
